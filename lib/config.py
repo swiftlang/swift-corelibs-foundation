d9cf62b023c9fe0ae0c30c2aeb6711e65b5e84d8
Philippe Hausler <phausler@apple.com>
2015-12-04 14:01:26 -0800

    Allow configurations to be reconfigured for pulling in source list changes after initial config pass
    After building from a full swift project build a configuration file is emitted to store the settings used for this build pass. When iterating with adding files or changing flags the ninja target `reconfigure` should be run to re-invoke the configure script to pick up on new changes to build.py

--- lib/config.py
+++ lib/config.py
@@ -7,9 +7,14 @@
 # See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 #
 
+import json
+
+from path import Path
+
 class Configuration:
     Debug = "debug"
     Release = "release"
+    version = 1
 
     command = None
     current = None
@@ -30,6 +35,7 @@ class Configuration:
     clangxx = None
     swift = None
     swiftc = None
+    ar = None
     swift_sdk = None
     bootstrap_directory = None
     verbose = False
@@ -37,6 +43,42 @@ class Configuration:
     extra_swift_flags = None
     extra_ld_flags = None
     build_mode = None
+    config_path = None # dont save this; else it would be recursive
     variables = {}
     def __init__(self):
         pass
+
+    def write(self, path):
+        info = {
+            'version' : self.version,
+            'command' : self.command,
+            'project' : self.project,
+            'script_path' : self.script_path.absolute(),
+            'build_script_path' : self.build_script_path.absolute(),
+            'source_root' : self.source_root.absolute(),
+            'target' : self.target.triple,
+            'system_root' : self.system_root.absolute(),
+            'toolchain' : self.toolchain,
+            'build_directory' : self.build_directory.absolute(),
+            'intermediate_directory' : self.intermediate_directory.absolute(),
+            'module_cache_directory' : self.module_cache_directory.absolute(),
+            'install_directory' : self.install_directory.absolute(),
+            'prefix' : self.prefix,
+            'swift_install' : self.swift_install,
+            'clang' : self.clang,
+            'clangxx' : self.clangxx,
+            'swift' : self.swift,
+            'swiftc' : self.swiftc,
+            'ar' : self.ar,
+            'swift_sdk' : self.swift_sdk,
+            'bootstrap_directory' : self.bootstrap_directory.absolute(),
+            'verbose' : self.verbose,
+            'extra_c_flags' : self.extra_c_flags,
+            'extra_swift_flags' : self.extra_swift_flags,
+            'extra_ld_flags' : self.extra_ld_flags,
+            'build_mode' : self.build_mode,
+            'variables' : self.variables,
+        }
+        with open(path, 'w+') as outfile:
+            json.dump(info, outfile)
+        
