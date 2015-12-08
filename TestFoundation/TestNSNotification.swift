61eb1487b4d95fd4c90ec99fdbafd7727f3eca44
Harlan Haskins <harlan@harlanhaskins.com>
2015-12-08 15:14:16 -0500

    Added another test for encodeWithCoder.

--- TestFoundation/TestNSNotification.swift
+++ TestFoundation/TestNSNotification.swift
@@ -24,22 +24,37 @@ class TestNSNotification: XCTestCase {
     var allTests: [(String, () -> ())] {
         return [
             ("test_copyWithZone", test_copyWithZone),
+            // TODO: Uncomment this once NSKeyed[Un]Archiver is implemented.
+            // ("test_encodeWithCoder", test_encodeWithCoder),
         ]
     }
     
-    func test_copyWithZone() {
-        let copy = notification.copyWithZone(nil) as! NSNotification
-        XCTAssertEqual(copy.name, notification.name)
-        guard let copyObject = copy.object, object = notification.object else {
+    func assertEqual(original: NSNotification, copy: NSNotification) {
+        XCTAssertEqual(copy.name, original.name)
+        guard let copyObject = copy.object, object = original.object else {
             XCTFail("Copy does not have an object")
             return
         }
-        guard let copyInfo = copy.userInfo, info = notification.userInfo else {
+        guard let copyInfo = copy.userInfo, info = original.userInfo else {
             XCTFail("Copy does not have a userInfo")
             return
         }
         XCTAssertEqual(ObjectIdentifier(copyObject), ObjectIdentifier(object))
         XCTAssertEqual(ObjectIdentifier(copyInfo), ObjectIdentifier(info))
+    }
+    
+    func test_copyWithZone() {
+        let copy = notification.copyWithZone(nil) as! NSNotification
+        assertEqual(notification, copy: copy)
+    }
+    
+    func test_encodeWithCoder() {
+        let archive = NSKeyedArchiver.archivedDataWithRootObject(notification)
+        guard let unarchived = NSKeyedUnarchiver.unarchiveObjectWithData(archive) as? NSNotification else {
+            XCTFail("Notification could not be unarchived.")
+            return
+        }
+        assertEqual(notification, copy: unarchived)
     }
 
 }
