195e113f410506afad7f301339b0aa2d19f1cdf3
Matt Rajca <matt.rajca@me.com>
2015-12-07 23:40:04 -0600

    Implement NSAffineTransform.transform{Point,Size}
    As part of this change:
    
    - The addition and multiplication operators have been implemented for CGFloat so we can compute dot products
    - A private helper function for matrix-vector products has been implemented

--- TestFoundation/TestNSAffineTransform.swift
+++ TestFoundation/TestNSAffineTransform.swift
@@ -24,10 +24,12 @@
 #endif
 
 class TestNSAffineTransform : XCTestCase {
+    private let accuracyThreshold = 0.001
 
     var allTests : [(String, () -> ())] {
         return [
-            ("test_BasicConstruction", test_BasicConstruction)
+            ("test_BasicConstruction", test_BasicConstruction),
+            ("test_IdentityTransformation", test_IdentityTransformation)
         ]
     }
 
@@ -37,12 +39,36 @@ class TestNSAffineTransform : XCTestCase {
 
         // The diagonal entries (1,1) and (2,2) of the identity matrix are ones. The other entries are zeros.
         // TODO: These should use DBL_MAX but it's not available as part of Glibc on Linux
-        XCTAssertEqualWithAccuracy(Double(transformStruct.m11), Double(1), accuracy: 0.001)
-        XCTAssertEqualWithAccuracy(Double(transformStruct.m22), Double(1), accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(Double(transformStruct.m11), Double(1), accuracy: accuracyThreshold)
+        XCTAssertEqualWithAccuracy(Double(transformStruct.m22), Double(1), accuracy: accuracyThreshold)
 
-        XCTAssertEqualWithAccuracy(Double(transformStruct.m12), Double(0), accuracy: 0.001)
-        XCTAssertEqualWithAccuracy(Double(transformStruct.m21), Double(0), accuracy: 0.001)
-        XCTAssertEqualWithAccuracy(Double(transformStruct.tX), Double(0), accuracy: 0.001)
-        XCTAssertEqualWithAccuracy(Double(transformStruct.tY), Double(0), accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(Double(transformStruct.m12), Double(0), accuracy: accuracyThreshold)
+        XCTAssertEqualWithAccuracy(Double(transformStruct.m21), Double(0), accuracy: accuracyThreshold)
+        XCTAssertEqualWithAccuracy(Double(transformStruct.tX), Double(0), accuracy: accuracyThreshold)
+        XCTAssertEqualWithAccuracy(Double(transformStruct.tY), Double(0), accuracy: accuracyThreshold)
+    }
+
+    func test_IdentityTransformation() {
+        let identityTransform = NSAffineTransform()
+
+        func checkIdentityPointTransformation(point: NSPoint) {
+            let newPoint = identityTransform.transformPoint(point)
+            XCTAssertEqualWithAccuracy(Double(newPoint.x), Double(point.x), accuracy: accuracyThreshold)
+            XCTAssertEqualWithAccuracy(Double(newPoint.y), Double(point.y), accuracy: accuracyThreshold)
+        }
+
+        checkIdentityPointTransformation(NSPoint())
+        checkIdentityPointTransformation(NSMakePoint(CGFloat(24.5), CGFloat(10.0)))
+        checkIdentityPointTransformation(NSMakePoint(CGFloat(-7.5), CGFloat(2.0)))
+
+        func checkIdentitySizeTransformation(size: NSSize) {
+            let newSize = identityTransform.transformSize(size)
+            XCTAssertEqualWithAccuracy(Double(newSize.width), Double(size.width), accuracy: accuracyThreshold)
+            XCTAssertEqualWithAccuracy(Double(newSize.height), Double(size.height), accuracy: accuracyThreshold)
+        }
+
+        checkIdentitySizeTransformation(NSSize())
+        checkIdentitySizeTransformation(NSMakeSize(CGFloat(13.0), CGFloat(12.5)))
+        checkIdentitySizeTransformation(NSMakeSize(CGFloat(100.0), CGFloat(-100.0)))
     }
 }
