54363b80066ee9a129e5ad84058b8ad783740830
Sergey Bolshedvorsky <sergey@bolshedvorsky.com>
2015-12-09 20:42:22 +0000

    Implementation of NSBytecountFormatter default values

--- Foundation/NSByteCountFormatter.swift
+++ Foundation/NSByteCountFormatter.swift
@@ -40,6 +40,10 @@ public enum NSByteCountFormatterCountStyle : Int {
 }
 
 public class NSByteCountFormatter : NSFormatter {
+    public override init() {
+        super.init()
+    }
+    
     public required init?(coder: NSCoder) {
         NSUnimplemented()
     }
@@ -54,35 +58,35 @@ public class NSByteCountFormatter : NSFormatter {
     
     /* Specify the units that can be used in the output. If NSByteCountFormatterUseDefault, uses platform-appropriate settings; otherwise will only use the specified units. This is the default value. Note that ZB and YB cannot be covered by the range of possible values, but you can still choose to use these units to get fractional display ("0.0035 ZB" for instance).
     */
-    public var allowedUnits: NSByteCountFormatterUnits
+    public var allowedUnits: NSByteCountFormatterUnits = .UseDefault
     
     /* Specify how the count is displayed by indicating the number of bytes to be used for kilobyte. The default setting is NSByteCountFormatterFileCount, which is the system specific value for file and storage sizes.
     */
-    public var countStyle: NSByteCountFormatterCountStyle
+    public var countStyle: NSByteCountFormatterCountStyle = .File
     
     /* Choose whether to allow more natural display of some values, such as zero, where it may be displayed as "Zero KB," ignoring all other flags or options (with the exception of NSByteCountFormatterUseBytes, which would generate "Zero bytes"). The result is appropriate for standalone output. Default value is YES. Special handling of certain values such as zero is especially important in some languages, so it's highly recommended that this property be left in its default state.
     */
-    public var allowsNonnumericFormatting: Bool
+    public var allowsNonnumericFormatting: Bool = true
     
     /* Choose whether to include the number or the units in the resulting formatted string. (For example, instead of 723 KB, returns "723" or "KB".) You can call the API twice to get both parts, separately. But note that putting them together yourself via string concatenation may be wrong for some locales; so use this functionality with care.  Both of these values are YES by default.  Setting both to NO will unsurprisingly result in an empty string.
     */
-    public var includesUnit: Bool
-    public var includesCount: Bool
+    public var includesUnit: Bool = true
+    public var includesCount: Bool = true
     
     /* Choose whether to parenthetically (localized as appropriate) display the actual number of bytes as well, for instance "723 KB (722,842 bytes)".  This will happen only if needed, that is, the first part is already not showing the exact byte count.  If includesUnit or includesCount are NO, then this setting has no effect.  Default value is NO.
     */
-    public var includesActualByteCount: Bool
+    public var includesActualByteCount: Bool = false
     
     /* Choose the display style. The "adaptive" algorithm is platform specific and uses a different number of fraction digits based on the magnitude (in 10.8: 0 fraction digits for bytes and KB; 1 fraction digits for MB; 2 for GB and above). Otherwise the result always tries to show at least three significant digits, introducing fraction digits as necessary. Default is YES.
     */
-    public var adaptive: Bool
+    public var adaptive: Bool = true
     
     /* Choose whether to zero pad fraction digits so a consistent number of fraction digits are displayed, causing updating displays to remain more stable. For instance, if the adaptive algorithm is used, this option formats 1.19 and 1.2 GB as "1.19 GB" and "1.20 GB" respectively, while without the option the latter would be displayed as "1.2 GB". Default value is NO.
     */
-    public var zeroPadsFractionDigits: Bool
+    public var zeroPadsFractionDigits: Bool = false
     
     /* Specify the formatting context for the formatted string. Default is NSFormattingContextUnknown.
     */
-    public var formattingContext: NSFormattingContext
+    public var formattingContext: NSFormattingContext = .Unknown
 }
 
