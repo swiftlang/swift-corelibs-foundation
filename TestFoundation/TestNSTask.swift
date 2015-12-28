7c4cb97102376bb5ab2465c7b8656e4412d33d72
“djs-code” <daniel.j.stenmark@gmail.com>
2015-12-28 00:59:00 -0500

    Baseline work on NSTask
    NSTask can now launch processes using a launch path and arguments, as well as trigger a termination handler.

---
+++ TestFoundation/TestNSTask.swift
@@ -0,0 +1,60 @@
+//
+//  TestNSTask.swift
+//  Foundation
+//
+//  Created by Daniel Stenmark on 12/27/15.
+//  Copyright © 2015 Apple. All rights reserved.
+//
+
+#if DEPLOYMENT_RUNTIME_OBJC || os(Linux)
+    import Foundation
+    import XCTest
+#else
+    import SwiftFoundation
+    import SwiftXCTest
+#endif
+import CoreFoundation
+
+class TestNSTask : XCTestCase {
+    var allTests: [(String, () -> Void)] {
+        return [("test_exit0" , test_exit0),
+                ("test_exit1" , test_exit1),
+                ("test_exit100" , test_exit100)]
+    }
+    
+    func test_exit0() {
+        
+        let task = NSTask()
+        
+        task.launchPath = "/bin/bash"
+        task.arguments = ["-c", "exit 0"]
+        
+        task.launch()
+        task.waitUntilExit()
+        XCTAssertEqual(task.terminationStatus, 0)
+    }
+    
+    func test_exit1() {
+        
+        let task = NSTask()
+        
+        task.launchPath = "/bin/bash"
+        task.arguments = ["-c", "exit 1"]
+
+        task.launch()
+        task.waitUntilExit()
+        XCTAssertEqual(task.terminationStatus, 1)
+    }
+    
+    func test_exit100() {
+        
+        let task = NSTask()
+        
+        task.launchPath = "/bin/bash"
+        task.arguments = ["-c", "exit 100"]
+        
+        task.launch()
+        task.waitUntilExit()
+        XCTAssertEqual(task.terminationStatus, 100)
+    }
+}
