17ace7bbd2460f38ea24a03a9bdbfab147faea4b
Philippe Hausler <phausler@apple.com>
2016-01-07 14:12:44 -0800

    update plutil to parse options more robustly and add display function and a placeholder for conversion

--- Tools/plutil/main.swift
+++ Tools/plutil/main.swift
@@ -14,7 +14,7 @@ import Foundation
 import Glibc
 #endif
 
-func help() {
+func help() -> Int32 {
     print("plutil: [command_option] [other_options] file...\n" +
     "The file '-' means stdin\n" +
     "Command options are (-lint is the default):\n" +
@@ -33,49 +33,128 @@ func help() {
     " -e extension                  specify alternate extension for converted files\n" +
     " -r                            if writing JSON, output in human-readable form\n" +
     " --                            specifies that all further arguments are file names\n")
+    return EXIT_SUCCESS
 }
 
-func lint(args : [String]) {
-    var silent = false
-    // Be nice and filter the rest of the arguments for other optional arguments
-    let filteredArgs = args.filter { arg in
+enum ExecutionMode {
+    case Help
+    case Lint
+    case Convert
+    case Print
+}
+
+enum ConversionFormat {
+    case XML1
+    case Binary1
+    case JSON
+}
+
+struct Options {
+    var mode: ExecutionMode = .Lint
+    var silent: Bool = false
+    var output: String?
+    var fileExtension: String?
+    var humanReadable: Bool?
+    var conversionFormat: ConversionFormat?
+    var inputs = [String]()
+}
+
+enum OptionParseError : ErrorType {
+    case UnrecognizedArgument(String)
+    case MissingArgument(String)
+    case InvalidFormat(String)
+}
+
+func parseArguments(args: [String]) throws -> Options {
+    var opts = Options()
+    var iterator = args.generate()
+    while let arg = iterator.next() {
         switch arg {
             case "--":
-                return false
-            
+                while let path = iterator.next() {
+                    opts.inputs.append(path)
+                }
+                break
             case "-s":
-                silent = true
-                return false
-            
+                opts.silent = true
+                break
             case "-o":
-                print("-o is not used with -lint")
-                help()
-                exit(EXIT_FAILURE)
-            
+                if let path = iterator.next() {
+                    opts.output = path
+                } else {
+                    throw OptionParseError.MissingArgument("-o requires a path argument")
+                }
+                break
+            case "-convert":
+                opts.mode = ExecutionMode.Convert
+                if let format = iterator.next() {
+                    switch format {
+                        case "xml1":
+                            opts.conversionFormat = ConversionFormat.XML1
+                            break
+                        case "binary1":
+                            opts.conversionFormat = ConversionFormat.Binary1
+                            break
+                        case "json":
+                            opts.conversionFormat = ConversionFormat.JSON
+                            break
+                        default:
+                            throw OptionParseError.InvalidFormat(format)
+                    }
+                } else {
+                    throw OptionParseError.MissingArgument("-convert requires a format argument of xml1 binary1 json")
+                }
+                break
             case "-e":
-                print("-e is not used with -lint")
-                help()
-                exit(EXIT_FAILURE)
-
+                if let ext = iterator.next() {
+                    opts.fileExtension = ext
+                } else {
+                    throw OptionParseError.MissingArgument("-e requires an extension argument")
+                }
+            case "-help":
+                opts.mode = ExecutionMode.Help
+                break
+            case "-lint":
+                opts.mode = ExecutionMode.Lint
+                break
+            case "-p":
+                opts.mode = ExecutionMode.Print
+                break
             default:
                 if arg.hasPrefix("-") && arg.utf8.count > 1 {
-                    print("unrecognized option \(arg)")
-                    help()
-                    exit(EXIT_FAILURE)
+                    throw OptionParseError.UnrecognizedArgument(arg)
                 }
-                return true
+                break
         }
-        return true
     }
     
-    if filteredArgs.count < 1 {
+    return opts
+}
+
+
+func lint(options: Options) -> Int32 {
+    if options.output != nil {
+        print("-o is not used with -lint")
+        help()
+        return EXIT_FAILURE
+    }
+    
+    if options.fileExtension != nil {
+        print("-e is not used with -lint")
+        help()
+        return EXIT_FAILURE
+    }
+    
+    if options.inputs.count < 1 {
         print("No files specified.")
         help()
-        exit(EXIT_FAILURE)
+        return EXIT_FAILURE
     }
     
+    let silent = options.silent
+    
     var doError = false
-    for file in filteredArgs {
+    for file in options.inputs {
         let data : NSData?
         if file == "-" {
             // stdin
@@ -103,7 +182,174 @@ func lint(args : [String]) {
     }
     
     if doError {
-        exit(EXIT_FAILURE)
+        return EXIT_FAILURE
+    } else {
+        return EXIT_SUCCESS
+    }
+}
+
+func convert(options: Options) -> Int32 {
+    print("Unimplemented")
+    return EXIT_FAILURE
+}
+
+enum DisplayType {
+    case Primary
+    case Key
+    case Value
+}
+
+extension Dictionary {
+    func display(indent: Int = 0, type: DisplayType = .Primary) {
+        let indentation = String(count: indent * 2, repeatedValue: Character(" "))
+        if type == .Primary || type == .Key {
+            print("\(indentation)[\n", terminator: "")
+        } else {
+            print("[\n", terminator: "")
+        }
+        
+        forEach() {
+            if let key = $0.0 as? String {
+                key.display(indent + 1, type: .Key)
+            } else {
+                fatalError("plists should have strings as keys but got a \($0.0.dynamicType)")
+            }
+            print(" => ", terminator: "")
+            displayPlist($0.1, indent: indent + 1, type: .Value)
+        }
+        
+        print("\(indentation)]\n", terminator: "")
+    }
+}
+
+extension Array {
+    func display(indent: Int = 0, type: DisplayType = .Primary) {
+        let indentation = String(count: indent * 2, repeatedValue: Character(" "))
+        if type == .Primary || type == .Key {
+            print("\(indentation)[\n", terminator: "")
+        } else {
+            print("[\n", terminator: "")
+        }
+        
+        for idx in 0..<count {
+            print("\(indentation)  \(idx) => ", terminator: "")
+            displayPlist(self[idx], indent: indent + 1, type: .Value)
+        }
+        
+        print("\(indentation)]\n", terminator: "")
+    }
+}
+
+extension String {
+    func display(indent: Int = 0, type: DisplayType = .Primary) {
+        let indentation = String(count: indent * 2, repeatedValue: Character(" "))
+        if type == .Primary {
+            print("\(indentation)\"\(self)\"\n", terminator: "")
+        }
+        else if type == .Key {
+            print("\(indentation)\"\(self)\"", terminator: "")
+        } else {
+            print("\"\(self)\"\n", terminator: "")
+        }
+    }
+}
+
+extension Bool {
+    func display(indent: Int = 0, type: DisplayType = .Primary) {
+        let indentation = String(count: indent * 2, repeatedValue: Character(" "))
+        if type == .Primary {
+            print("\(indentation)\"\(self ? "1" : "0")\"\n", terminator: "")
+        }
+        else if type == .Key {
+            print("\(indentation)\"\(self ? "1" : "0")\"", terminator: "")
+        } else {
+            print("\"\(self ? "1" : "0")\"\n", terminator: "")
+        }
+    }
+}
+
+extension NSNumber {
+    func display(indent: Int = 0, type: DisplayType = .Primary) {
+        let indentation = String(count: indent * 2, repeatedValue: Character(" "))
+        if type == .Primary {
+            print("\(indentation)\"\(self)\"\n", terminator: "")
+        }
+        else if type == .Key {
+            print("\(indentation)\"\(self)\"", terminator: "")
+        } else {
+            print("\"\(self)\"\n", terminator: "")
+        }
+    }
+}
+
+extension NSData {
+    func display(indent: Int = 0, type: DisplayType = .Primary) {
+        let indentation = String(count: indent * 2, repeatedValue: Character(" "))
+        if type == .Primary {
+            print("\(indentation)\"\(self)\"\n", terminator: "")
+        }
+        else if type == .Key {
+            print("\(indentation)\"\(self)\"", terminator: "")
+        } else {
+            print("\"\(self)\"\n", terminator: "")
+        }
+    }
+}
+
+func displayPlist(plist: Any, indent: Int = 0, type: DisplayType = .Primary) {
+    if let val = plist as? Dictionary<String, Any> {
+        val.display(indent, type: type)
+    } else if let val = plist as? Array<Any> {
+        val.display(indent, type: type)
+    } else if let val = plist as? String {
+        val.display(indent, type: type)
+    } else if let val = plist as? Bool {
+        val.display(indent, type: type)
+    } else if let val = plist as? NSNumber {
+        val.display(indent, type: type)
+    } else if let val = plist as? NSData {
+        val.display(indent, type: type)
+    } else {
+        fatalError("unhandled type \(plist.dynamicType)")
+    }
+}
+
+func display(options: Options) -> Int32 {
+    if options.inputs.count < 1 {
+        print("No files specified.")
+        help()
+        return EXIT_FAILURE
+    }
+    
+    var doError = false
+    for file in options.inputs {
+        let data : NSData?
+        if file == "-" {
+            // stdin
+            data = NSFileHandle.fileHandleWithStandardInput().readDataToEndOfFile()
+        } else {
+            data = NSData(contentsOfFile: file)
+        }
+        
+        if let d = data {
+            do {
+                let plist = try NSPropertyListSerialization.propertyListWithData(d, options: [], format: nil)
+                displayPlist(plist)
+            } catch {
+                print("\(file): \(error)")
+            }
+            
+        } else {
+            print("\(file) does not exists or is not readable or is not a regular file")
+            doError = true
+            continue
+        }
+    }
+    
+    if doError {
+        return EXIT_FAILURE
+    } else {
+        return EXIT_SUCCESS
     }
 }
 
@@ -117,25 +363,33 @@ func main() -> Int32 {
     
     // Throw away process path
     args.removeFirst()
-    
-    let command = args[0]
-    switch command {
-        case "-help":
-            help()
-            return EXIT_SUCCESS
-
-        case "-lint":
-            // Throw away command arg
-            args.removeFirst()
-            lint(args)
-        
-        default:
-            // Default is to lint
-            lint(args)
+    do {
+        let opts = try parseArguments(args)
+        switch opts.mode {
+            case .Lint:
+                return lint(opts)
+            case .Convert:
+                return convert(opts)
+            case .Print:
+                return display(opts)
+            case .Help:
+                return help()
+        }
+    } catch let err {
+        switch err as! OptionParseError {
+            case .UnrecognizedArgument(let arg):
+                print("unrecognized option: \(arg)")
+                help()
+                break
+            case .InvalidFormat(let format):
+                print("unrecognized format \(format)\nformat should be one of: xml1 binary1 json")
+                break
+            case .MissingArgument(let errorStr):
+                print(errorStr)
+                break
+        }
+        return EXIT_FAILURE
     }
-    
-    return EXIT_SUCCESS
-    
 }
 
 exit(main())
