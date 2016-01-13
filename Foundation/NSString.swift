14b59b68850e1e37295d2fdd8ce55d84fa7b7493
Peter Borg <pgw3@mac.com>
2016-01-13 19:22:03 +0100

    Refactored it so contentsOfFile calls contentsOfURL to avoid duplicated logic

--- Foundation/NSString.swift
+++ Foundation/NSString.swift
@@ -1274,30 +1274,9 @@ extension NSString {
             return nil
         }
     }
-    
+
     public convenience init(contentsOfURL url: NSURL, encoding enc: UInt) throws {
-        if url.fileURL, let path = url.path {
-            try self.init(contentsOfFile: path, encoding: enc)
-        } else {
-            let readResult = try NSData.init(contentsOfURL: url, options: [])
-            guard let cf = CFStringCreateWithBytes(kCFAllocatorDefault, UnsafePointer<UInt8>(readResult.bytes), readResult.length, CFStringConvertNSStringEncodingToEncoding(enc), true) else {
-                throw NSError(domain: NSCocoaErrorDomain, code: NSCocoaError.FileReadInapplicableStringEncodingError.rawValue, userInfo: [
-                    "NSDebugDescription" : "Unable to create a string using the specified encoding."
-                    ])
-            }
-            var str: String?
-            if String._conditionallyBridgeFromObject(cf._nsObject, result: &str) {
-                self.init(str!)
-            } else {
-                throw NSError(domain: NSCocoaErrorDomain, code: NSCocoaError.FileReadInapplicableStringEncodingError.rawValue, userInfo: [
-                    "NSDebugDescription" : "Unable to bridge CFString to String."
-                    ])
-            }
-        }
-    }
-    
-    public convenience init(contentsOfFile path: String, encoding enc: UInt) throws {
-        let readResult = try NSData.readBytesFromFileWithExtendedAttributes(path, options: [])
+        let readResult = try NSData.init(contentsOfURL: url, options: [])
         guard let cf = CFStringCreateWithBytes(kCFAllocatorDefault, UnsafePointer<UInt8>(readResult.bytes), readResult.length, CFStringConvertNSStringEncodingToEncoding(enc), true) else {
             throw NSError(domain: NSCocoaErrorDomain, code: NSCocoaError.FileReadInapplicableStringEncodingError.rawValue, userInfo: [
                 "NSDebugDescription" : "Unable to create a string using the specified encoding."
@@ -1311,6 +1290,10 @@ extension NSString {
                 "NSDebugDescription" : "Unable to bridge CFString to String."
                 ])
         }
+    }
+
+    public convenience init(contentsOfFile path: String, encoding enc: UInt) throws {
+        try self.init(contentsOfURL: NSURL(fileURLWithPath: path), encoding: enc)
     }
     
     public convenience init(contentsOfURL url: NSURL, usedEncoding enc: UnsafeMutablePointer<UInt>) throws {
