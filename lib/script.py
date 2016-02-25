4979968385008ea45a27ef6233be1321045afe1b
Philippe Hausler <phausler@apple.com>
2016-02-25 12:46:36 -0800

    O is for optimized: add the optimized flag for release builds

--- lib/script.py
+++ lib/script.py
@@ -111,7 +111,7 @@ TARGET_CFLAGS         = -fcolor-diagnostics -fdollars-in-identifiers -fblocks -f
         if Configuration.current.build_mode == Configuration.Debug:
             swift_flags += "-g -Onone "
         elif Configuration.current.build_mode == Configuration.Release:
-            swift_flags += " " 
+            swift_flags += "-O "
 
         swift_flags += Configuration.current.extra_swift_flags
         
