d474259d360e979ffb93a28bb2f7b0b8436e797a
seabaylea <baileyc@uk.ibm.com>
2016-03-07 20:08:29 +0000

    Implement missing stringValue and descriptionWithLocale APIs

--- Foundation/NSDecimalNumber.swift
+++ Foundation/NSDecimalNumber.swift
@@ -55,7 +55,7 @@ public class NSDecimalNumber : NSNumber {
         NSRequiresConcreteImplementation()
     }
     
-    public func descriptionWithLocale(locale: AnyObject?) -> String { NSUnimplemented() }
+    public override func descriptionWithLocale(locale: AnyObject?) -> String { NSUnimplemented() }
     
     // TODO: "declarations from extensions cannot be overridden yet"
     // Although it's not clear we actually need to redeclare this here when the extension adds it to the superclass of this class
