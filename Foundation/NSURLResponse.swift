3f5a07012cb9d27c5be6d3d9d98f9ba13866b53d
Daniel Eggert <daniel.eggert@hoerbuch.dk>
2016-04-15 20:31:32 +0200

    Convert ObjC comments into Swift documentation markup. (#321)

--- Foundation/NSURLResponse.swift
+++ Foundation/NSURLResponse.swift
@@ -8,18 +8,14 @@
 //
 
 
-/*!
-    @class NSURLResponse
-
-    @abstract An NSURLResponse object represents a URL load response in a
-    manner independent of protocol and URL scheme.
-
-    @discussion NSURLResponse encapsulates the metadata associated
-    with a URL load. Note that NSURLResponse objects do not contain
-    the actual bytes representing the content of a URL. See
-    NSURLConnection and NSURLConnectionDelegate for more information
-    about receiving the content data for a URL load.
-*/
+/// An `NSURLResponse` object represents a URL load response in a
+/// manner independent of protocol and URL scheme.
+///
+/// `NSURLResponse` encapsulates the metadata associated
+/// with a URL load. Note that NSURLResponse objects do not contain
+/// the actual bytes representing the content of a URL. See
+/// `NSURLSession` for more information about receiving the content
+/// data for a URL load.
 public class NSURLResponse : NSObject, NSSecureCoding, NSCopying {
 
     static public func supportsSecureCoding() -> Bool {
@@ -42,16 +38,14 @@ public class NSURLResponse : NSObject, NSSecureCoding, NSCopying {
         NSUnimplemented()
     }
     
-    /*!
-        @method initWithURL:MIMEType:expectedContentLength:textEncodingName:
-        @abstract Initialize an NSURLResponse with the provided values.
-        @param URL the URL
-        @param MIMETYPE the MIME content type of the response
-        @param expectedContentLength the expected content length of the associated data
-        @param textEncodingName the name of the text encoding for the associated data, if applicable, else nil
-        @result The initialized NSURLResponse.
-        @discussion This is the designated initializer for NSURLResponse.
-    */
+    /// Initialize an NSURLResponse with the provided values.
+    ///
+    /// This is the designated initializer for NSURLResponse.
+    /// - Parameter URL: the URL
+    /// - Parameter mimeType: the MIME content type of the response
+    /// - Parameter expectedContentLength: the expected content length of the associated data
+    /// - Parameter textEncodingName the name of the text encoding for the associated data, if applicable, else nil
+    /// - Returns: The initialized NSURLResponse.
     public init(url: NSURL, mimeType: String?, expectedContentLength length: Int, textEncodingName name: String?) {
         self.url = url
         self.mimeType = mimeType
@@ -61,90 +55,73 @@ public class NSURLResponse : NSObject, NSSecureCoding, NSCopying {
         self.suggestedFilename = (c?.isEmpty ?? true) ? "Unknown" : c
     }
     
-    /*! 
-        @method URL
-        @abstract Returns the URL of the receiver. 
-        @result The URL of the receiver. 
-    */
+    /// The URL of the receiver.
     /*@NSCopying*/ public private(set) var url: NSURL?
 
     
-    /*! 
-        @method MIMEType
-        @abstract Returns the MIME type of the receiver. 
-        @discussion The MIME type is based on the information provided
-        from an origin source. However, that value may be changed or
-        corrected by a protocol implementation if it can be determined
-        that the origin server or source reported the information
-        incorrectly or imprecisely. An attempt to guess the MIME type may
-        be made if the origin source did not report any such information.
-        @result The MIME type of the receiver.
-    */
+    /// The MIME type of the receiver.
+    ///
+    /// The MIME type is based on the information provided
+    /// from an origin source. However, that value may be changed or
+    /// corrected by a protocol implementation if it can be determined
+    /// that the origin server or source reported the information
+    /// incorrectly or imprecisely. An attempt to guess the MIME type may
+    /// be made if the origin source did not report any such information.
     public private(set) var mimeType: String?
     
-    /*! 
-        @method expectedContentLength
-        @abstract Returns the expected content length of the receiver.
-        @discussion Some protocol implementations report a content length
-        as part of delivering load metadata, but not all protocols
-        guarantee the amount of data that will be delivered in actuality.
-        Hence, this method returns an expected amount. Clients should use
-        this value as an advisory, and should be prepared to deal with
-        either more or less data.
-        @result The expected content length of the receiver, or -1 if
-        there is no expectation that can be arrived at regarding expected
-        content length.
-    */
+    /// The expected content length of the receiver.
+    ///
+    /// Some protocol implementations report a content length
+    /// as part of delivering load metadata, but not all protocols
+    /// guarantee the amount of data that will be delivered in actuality.
+    /// Hence, this method returns an expected amount. Clients should use
+    /// this value as an advisory, and should be prepared to deal with
+    /// either more or less data.
+    ///
+    /// The expected content length of the receiver, or `-1` if
+    /// there is no expectation that can be arrived at regarding expected
+    /// content length.
     public private(set) var expectedContentLength: Int64
     
-    /*! 
-        @method textEncodingName
-        @abstract Returns the name of the text encoding of the receiver.
-        @discussion This name will be the actual string reported by the
-        origin source during the course of performing a protocol-specific
-        URL load. Clients can inspect this string and convert it to an
-        NSStringEncoding or CFStringEncoding using the methods and
-        functions made available in the appropriate framework.
-        @result The name of the text encoding of the receiver, or nil if no
-        text encoding was specified. 
-    */
+    /// The name of the text encoding of the receiver.
+    ///
+    /// This name will be the actual string reported by the
+    /// origin source during the course of performing a protocol-specific
+    /// URL load. Clients can inspect this string and convert it to an
+    /// NSStringEncoding or CFStringEncoding using the methods and
+    /// functions made available in the appropriate framework.
     public private(set) var textEncodingName: String?
     
-    /*!
-        @method suggestedFilename
-        @abstract Returns a suggested filename if the resource were saved to disk.
-        @discussion The method first checks if the server has specified a filename using the
-        content disposition header. If no valid filename is specified using that mechanism,
-        this method checks the last path component of the URL. If no valid filename can be
-        obtained using the last path component, this method uses the URL's host as the filename.
-        If the URL's host can't be converted to a valid filename, the filename "unknown" is used.
-        In mose cases, this method appends the proper file extension based on the MIME type.
-        This method always returns a valid filename.
-        @result A suggested filename to use if saving the resource to disk.
-    */
+    /// A suggested filename if the resource were saved to disk.
+    ///
+    /// The method first checks if the server has specified a filename
+    /// using the content disposition header. If no valid filename is
+    /// specified using that mechanism, this method checks the last path
+    /// component of the URL. If no valid filename can be obtained using
+    /// the last path component, this method uses the URL's host as the
+    /// filename. If the URL's host can't be converted to a valid
+    /// filename, the filename "unknown" is used. In mose cases, this
+    /// method appends the proper file extension based on the MIME type.
+    ///
+    /// This method always returns a valid filename.
     public private(set) var suggestedFilename: String?
 }
 
-/*!
-    @class NSHTTPURLResponse
-
-    @abstract An NSHTTPURLResponse object represents a response to an
-    HTTP URL load. It is a specialization of NSURLResponse which
-    provides conveniences for accessing information specific to HTTP
-    protocol responses.
-*/
+/// A Response to an HTTP URL load.
+///
+/// An NSHTTPURLResponse object represents a response to an
+/// HTTP URL load. It is a specialization of NSURLResponse which
+/// provides conveniences for accessing information specific to HTTP
+/// protocol responses.
 public class NSHTTPURLResponse : NSURLResponse {
     
-    /*!
-      @method	initWithURL:statusCode:HTTPVersion:headerFields:
-      @abstract initializer for NSHTTPURLResponse objects.
-      @param 	url the URL from which the response was generated.
-      @param	statusCode an HTTP status code.
-      @param	HTTPVersion The version of the HTTP response as represented by the server.  This is typically represented as "HTTP/1.1".
-      @param 	headerFields A dictionary representing the header keys and values of the server response.
-      @result 	the instance of the object, or NULL if an error occurred during initialization.
-      @discussion This API was introduced in Mac OS X 10.7.2 and iOS 5.0 and is not available prior to those releases.
-    */
+    /// Initializer for NSHTTPURLResponse objects.
+    ///
+    /// - Parameter url: the URL from which the response was generated.
+    /// - Parameter statusCode: an HTTP status code.
+    /// - Parameter httpVersion: The version of the HTTP response as represented by the server.  This is typically represented as "HTTP/1.1".
+    /// - Parameter headerFields: A dictionary representing the header keys and values of the server response.
+    /// - Returns: the instance of the object, or `nil` if an error occurred during initialization.
     public init?(url: NSURL, statusCode: Int, httpVersion: String?, headerFields: [String : String]?) {
         self.statusCode = statusCode
         self.allHeaderFields = headerFields ?? [:]
@@ -161,11 +138,7 @@ public class NSHTTPURLResponse : NSURLResponse {
         NSUnimplemented()
     }
     
-    /*! 
-        @method statusCode
-        @abstract Returns the HTTP status code of the receiver. 
-        @result The HTTP status code of the receiver. 
-    */
+    /// The HTTP status code of the receiver.
     public let statusCode: Int
     
     /// Returns a dictionary containing all the HTTP header fields
@@ -183,13 +156,9 @@ public class NSHTTPURLResponse : NSURLResponse {
     /// `[NSObject: AnyObject]` type that Darwin Foundation uses.
     public let allHeaderFields: [String: String]
     
-    /*!
-        @method localizedStringForStatusCode:
-        @abstract Convenience method which returns a localized string
-        corresponding to the status code for this response.
-        @param the status code to use to produce a localized string.
-        @result A localized string corresponding to the given status code.
-    */
+    /// Convenience method which returns a localized string
+    /// corresponding to the status code for this response.
+    /// - Parameter forStatusCode: the status code to use to produce a localized string.
     public class func localizedString(forStatusCode statusCode: Int) -> String {
         switch statusCode {
         case 100: return "Continue"
@@ -260,6 +229,7 @@ public class NSHTTPURLResponse : NSURLResponse {
         }
     }
 }
+
 /// Parses the expected content length from the headers.
 ///
 /// Note that the message content length is different from the message
