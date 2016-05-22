bd4ffc9716ced3a789a22fadf7df473e59d520d3
David Hart <david@hartbit.com>
2016-05-22 23:46:21 +0200

    initial work on implemented unit counts

--- Foundation/NSProgress.swift
+++ Foundation/NSProgress.swift
@@ -28,15 +28,25 @@ public class NSProgress : NSObject {
     
     /* The instance of NSProgress associated with the current thread by a previous invocation of -becomeCurrentWithPendingUnitCount:, if any. The purpose of this per-thread value is to allow code that does work to usefully report progress even when it is widely separated from the code that actually presents progress to the user, without requiring layers of intervening code to pass the instance of NSProgress through. Using the result of invoking this directly will often not be the right thing to do, because the invoking code will often not even know what units of work the current progress object deals in. Invoking +progressWithTotalUnitCount: to create a child NSProgress object and then using that to report progress makes more sense in that situation.
     */
-    public class func current() -> NSProgress? { NSUnimplemented() }
+    public class func current() -> NSProgress? {
+        return NSProgress._currentProgress.get { NullableProgress() }.progress
+    }
+
+    /* Return an instance of NSProgress that has been initialized with -initWithParent:userInfo:. The initializer is passed the current progress object, if there is one. In many cases you can simply precede code that does a substantial amount of work with an invocation of this method, with repeated invocations of -setCompletedUnitCount: and -isCancelled in the loop that does the work.
+     
+     You can invoke this method on one thread and then message the returned NSProgress on another thread. For example, you can let the result of invoking this method get captured by a block passed to Dispatch.async(). In that block you can invoke methods like -becomeCurrentWithPendingUnitCount: and -resignCurrent, or -setCompletedUnitCount: and -isCancelled.
+     */
+    override convenience public init() {
+        self.init(parent: nil, userInfo: nil)
+    }
     
     /* Return an instance of NSProgress that has been initialized with -initWithParent:userInfo:. The initializer is passed the current progress object, if there is one, and the value of the totalUnitCount property is set. In many cases you can simply precede code that does a substantial amount of work with an invocation of this method, with repeated invocations of -setCompletedUnitCount: and -isCancelled in the loop that does the work.
     
-    You can invoke this method on one thread and then message the returned NSProgress on another thread. For example, you can let the result of invoking this method get captured by a block passed to dispatch_async(). In that block you can invoke methods like -becomeCurrentWithPendingUnitCount: and -resignCurrent, or -setCompletedUnitCount: and -isCancelled.
+    You can invoke this method on one thread and then message the returned NSProgress on another thread. For example, you can let the result of invoking this method get captured by a block passed to Dispatch.async(). In that block you can invoke methods like -becomeCurrentWithPendingUnitCount: and -resignCurrent, or -setCompletedUnitCount: and -isCancelled.
     */
-    public init(totalUnitCount unitCount: Int64) {
-        totalUnitCount = unitCount
-        completedUnitCount = 0
+    convenience public init(totalUnitCount unitCount: Int64) {
+        self.init(parent: nil, userInfo: nil)
+        self.totalUnitCount = unitCount
     }
     
     /* Return an instance of NSProgress that has been initialized with -initWithParent:userInfo:. The initializer is passed nil for the parent, resulting in a progress object that is not part of an existing progress tree. The value of the totalUnitCount property is also set.
@@ -49,17 +59,23 @@ public class NSProgress : NSObject {
     
     /* The designated initializer. If a parent NSProgress object is passed then progress reporting and cancellation checking done by the receiver will notify or consult the parent. The only valid arguments to the first argument of this method are nil (indicating no parent) or [NSProgress currentProgress]. Any other value will throw an exception.
     */
-    public init(parent parentProgressOrNil: NSProgress?, userInfo userInfoOrNil: [NSObject : AnyObject]? = [:]) { NSUnimplemented() }
+    public init(parent parentProgressOrNil: NSProgress?, userInfo userInfoOrNil: [NSObject : AnyObject]? = [:]) {
+        userInfo = userInfoOrNil ?? [:]
+    }
     
     /* Make the receiver the current thread's current progress object, returned by +currentProgress. At the same time, record how large a portion of the work represented by the receiver will be represented by the next progress object initialized by invoking -initWithParent:userInfo: in the current thread with the receiver as the parent. This will be used when that child is sent -setCompletedUnitCount: and the receiver is notified of that.
      
        With this mechanism, code that doesn't know anything about its callers can report progress accurately by using +progressWithTotalUnitCount: and -setCompletedUnitCount:. The calling code will account for the fact that the work done is only a portion of the work to be done as part of a larger operation. The unit of work in a call to -becomeCurrentWithPendingUnitCount: has to be the same unit of work as that used for the value of the totalUnitCount property, but the unit of work used by the child can be a completely different one, and often will be. You must always balance invocations of this method with invocations of -resignCurrent.
     */
-    public func becomeCurrent(withPendingUnitCount unitCount: Int64) { NSUnimplemented() }
+    public func becomeCurrent(withPendingUnitCount unitCount: Int64) {
+        NSProgress.setCurrent(progress: self)
+    }
     
     /* Balance the most recent previous invocation of -becomeCurrentWithPendingUnitCount: on the same thread by restoring the current progress object to what it was before -becomeCurrentWithPendingUnitCount: was invoked.
     */
-    public func resignCurrent() { NSUnimplemented() }
+    public func resignCurrent() {
+        NSProgress.setCurrent(progress: nil)
+    }
     
     /* Directly add a child progress to the receiver, assigning it a portion of the receiver's total unit count.
      */
@@ -69,8 +85,8 @@ public class NSProgress : NSObject {
      
        If the receiver NSProgress object is a "leaf progress" (no children), then the fractionCompleted is generally completedUnitCount / totalUnitCount. If the receiver NSProgress has children, the fractionCompleted will reflect progress made in child objects in addition to its own completedUnitCount. As children finish, the completedUnitCount of the parent will be updated.
     */
-    public var totalUnitCount: Int64
-    public var completedUnitCount: Int64
+    public var totalUnitCount: Int64 = 0
+    public var completedUnitCount: Int64 = 0
     
     /* A description of what progress is being made, fit to present to the user. NSProgress is by default KVO-compliant for this property, with the notifications always being sent on thread which updates the property. The default implementation of the getter for this property does not always return the most recently set value of the property. If the most recently set value of this property is nil then NSProgress uses the value of the kind property to determine how to use the values of other properties, as well as values in the user info dictionary, to return a computed string. If it fails to do that then it returns an empty string.
       
@@ -121,11 +137,21 @@ public class NSProgress : NSObject {
     
     /* Whether the progress being made is indeterminate. -isIndeterminate returns YES when the value of the totalUnitCount or completedUnitCount property is less than zero. Zero values for both of those properties indicates that there turned out to not be any work to do after all; -isIndeterminate returns NO and -fractionCompleted returns 1.0 in that case. NSProgress is by default KVO-compliant for these properties, with the notifications always being sent on the thread which updates the property.
     */
-    public var isIndeterminate: Bool { NSUnimplemented() }
+    public var isIndeterminate: Bool {
+        return completedUnitCount <= 0 && totalUnitCount <= 0
+    }
     
     /* The fraction of the overall work completed by this progress object, including work done by any children it may have.
     */
-    public var fractionCompleted: Double { NSUnimplemented() }
+    public var fractionCompleted: Double {
+        if completedUnitCount <= 0 || totalUnitCount < 0 {
+            return 0.0
+        } else if totalUnitCount == 0 {
+            return 1.0
+        } else {
+            return Double(completedUnitCount) / Double(totalUnitCount)
+        }
+    }
     
     /* Invoke the block registered with the cancellationHandler property, if there is one, and set the cancelled property to YES. Do this for the receiver, any descendants of the receiver, the instance of NSProgress that was published in another process to make the receiver if that's the case, and any descendants of such a published instance of NSProgress.
     */
@@ -141,7 +167,7 @@ public class NSProgress : NSObject {
     
     /* Arbitrary values associated with the receiver. Returns a KVO-compliant dictionary that changes as -setUserInfoObject:forKey: is sent to the receiver. The dictionary will send all of its KVO notifications on the thread which updates the property. The result will never be nil, but may be an empty dictionary. Some entries have meanings that are recognized by the NSProgress class itself. See the NSProgress...Key string constants listed below.
     */
-    public var userInfo: [NSObject : AnyObject] { NSUnimplemented() }
+    public var userInfo: [NSObject : AnyObject]
     
     /* Either a string identifying what kind of progress is being made, like NSProgressKindFile, or nil. If the value of the localizedDescription property has not been set to a non-nil value then the default implementation of -localizedDescription uses the progress kind to determine how to use the values of other properties, as well as values in the user info dictionary, to create a string that is presentable to the user. This is most useful when -localizedDescription is actually being invoked in another process, whose localization language may be different, as a result of using the publish and subscribe mechanism described here.
     */
@@ -187,5 +213,13 @@ public let NSProgressFileURLKey: String = "NSProgressFileURLKey"
 public let NSProgressFileTotalCountKey: String = "NSProgressFileTotalCountKey"
 public let NSProgressFileCompletedCountKey: String = "NSProgressFileCompletedCountKey"
 
+internal extension NSProgress {
+    static var _currentProgress = NSThreadSpecific<NullableProgress>()
+    class func setCurrent(progress: NSProgress?) {
+        NSProgress._currentProgress.get { NullableProgress() }.progress = progress
+    }
+}
 
-
+internal class NullableProgress: NSObject {
+    var progress: NSProgress?
+}
--- TestFoundation/TestNSProgress.swift
+++ TestFoundation/TestNSProgress.swift
@@ -19,16 +19,174 @@ class TestNSProgress: XCTestCase {
 
     static var allTests: [(String, (TestNSProgress) -> () throws -> Void)] {
         return [
-            ("test_initWithTotalUnitCount", test_initWithTotalUnitCount),
+            ("test_initWithTotalUnitCount", test_init),
+            ("test_initWithUserInfo", test_initWithUserInfo),
+            ("test_totalUnitCount", test_totalUnitCount),
+            ("test_completedUnitCount", test_completedUnitCount),
+            ("test_fractionCompleted", test_fractionCompleted),
+            ("test_isIndeterminate", test_isIndeterminate),
+            ("test_kind", test_kind),
+            ("test_currentProgress", test_currentProgress)
         ]
     }
     
-    func test_initWithTotalUnitCount() {
-        let progress = NSProgress(totalUnitCount: 1000)
-        XCTAssertEqual(progress.totalUnitCount, 1000)
+    func test_init() {
+        let progress = NSProgress()
+        XCTAssertTrue(progress.userInfo.isEmpty)
+        XCTAssertEqual(progress.totalUnitCount, 0)
         XCTAssertEqual(progress.completedUnitCount, 0)
         XCTAssertTrue(progress.isCancellable)
         XCTAssertFalse(progress.isPausable)
     }
+    
+    func test_initWithUserInfo() {
+        var progress = NSProgress(parent: nil, userInfo: nil)
+        XCTAssertTrue(progress.userInfo.isEmpty)
+        XCTAssertEqual(progress.totalUnitCount, 0)
+        XCTAssertEqual(progress.completedUnitCount, 0)
+        XCTAssertTrue(progress.isCancellable)
+        XCTAssertFalse(progress.isPausable)
+        
+        progress = NSProgress(parent: nil, userInfo: ["key".bridge(): "value".bridge()])
+        XCTAssertTrue(progress.userInfo.count == 1)
+        XCTAssertEqual(progress.userInfo["key".bridge()] as? NSString, "value".bridge())
+        XCTAssertEqual(progress.totalUnitCount, 0)
+        XCTAssertEqual(progress.completedUnitCount, 0)
+        XCTAssertTrue(progress.isCancellable)
+        XCTAssertFalse(progress.isPausable)
+    }
+    
+    func test_totalUnitCount() {
+        let progress = NSProgress(parent: nil, userInfo: nil)
+        progress.totalUnitCount = 100
+        XCTAssertEqual(progress.totalUnitCount, 100)
+    }
+    
+    func test_completedUnitCount() {
+        let progress = NSProgress(parent: nil, userInfo: nil)
+        progress.completedUnitCount = 50
+        XCTAssertEqual(progress.completedUnitCount, 50)
+    }
+    
+    func test_fractionCompleted() {
+        let progress = NSProgress(parent: nil, userInfo: nil)
+        
+        progress.completedUnitCount = -4
+        progress.totalUnitCount = -10
+        XCTAssertEqual(progress.fractionCompleted, 0)
+        
+        progress.completedUnitCount = -20
+        progress.totalUnitCount = -10
+        XCTAssertEqual(progress.fractionCompleted, 0)
+        
+        progress.completedUnitCount = -10
+        progress.totalUnitCount = 0
+        XCTAssertEqual(progress.fractionCompleted, 0)
+        
+        progress.completedUnitCount = 0
+        progress.totalUnitCount = -20
+        XCTAssertEqual(progress.fractionCompleted, 0)
+        
+        progress.completedUnitCount = 5
+        progress.totalUnitCount = -10
+        XCTAssertEqual(progress.fractionCompleted, 0)
+        
+        progress.completedUnitCount = -5
+        progress.totalUnitCount = 10
+        XCTAssertEqual(progress.fractionCompleted, 0)
+        
+        progress.completedUnitCount = 0
+        progress.totalUnitCount = 0
+        XCTAssertEqual(progress.fractionCompleted, 0)
+        
+        progress.completedUnitCount = 20
+        progress.totalUnitCount = 0
+        XCTAssertEqual(progress.fractionCompleted, 1.0)
+        
+        progress.completedUnitCount = 0
+        progress.totalUnitCount = 100
+        XCTAssertEqual(progress.fractionCompleted, 0)
+        
+        progress.completedUnitCount = 20
+        progress.totalUnitCount = 100
+        XCTAssertEqual(progress.fractionCompleted, 0.2)
+        
+        progress.completedUnitCount = 150
+        progress.totalUnitCount = 100
+        XCTAssertEqual(progress.fractionCompleted, 1.5)
+    }
+    
+    func test_isIndeterminate() {
+        let progress = NSProgress(parent: nil, userInfo: nil)
+        
+        progress.completedUnitCount = -4
+        progress.totalUnitCount = -10
+        XCTAssertTrue(progress.isIndeterminate)
+        
+        progress.completedUnitCount = -10
+        progress.totalUnitCount = 0
+        XCTAssertTrue(progress.isIndeterminate)
+        
+        progress.completedUnitCount = 0
+        progress.totalUnitCount = -20
+        XCTAssertTrue(progress.isIndeterminate)
+        
+        progress.completedUnitCount = 0
+        progress.totalUnitCount = 0
+        XCTAssertTrue(progress.isIndeterminate)
+        
+        progress.completedUnitCount = 20
+        progress.totalUnitCount = 0
+        XCTAssertFalse(progress.isIndeterminate)
+        
+        progress.completedUnitCount = 0
+        progress.totalUnitCount = 100
+        XCTAssertFalse(progress.isIndeterminate)
+        
+        progress.completedUnitCount = 20
+        progress.totalUnitCount = 100
+        XCTAssertFalse(progress.isIndeterminate)
+    }
+    
+    func test_kind() {
+        let progress = NSProgress(parent: nil, userInfo: nil)
+        
+        progress.kind = "custom"
+        XCTAssertEqual(progress.kind, "custom")
+        progress.kind = NSProgressKindFile
+        XCTAssertEqual(progress.kind, NSProgressKindFile)
+    }
+    
+    func test_currentProgress() {
+        let progress = NSProgress()
+        
+        XCTAssertNil(NSProgress.current())
+        XCTAssertTrue(isTrueInThread { NSProgress.current() == nil })
+        
+        progress.becomeCurrent(withPendingUnitCount: 0)
+        
+        XCTAssertEqual(NSProgress.current(), progress)
+        XCTAssertTrue(isTrueInThread { NSProgress.current() == nil })
+        
+        progress.resignCurrent()
+        
+        XCTAssertNil(NSProgress.current())
+        XCTAssertTrue(isTrueInThread { NSProgress.current() == nil })
+    }
 
+}
+
+func isTrueInThread(predicate: () -> Bool) -> Bool {
+    var result: Bool = false
+    let thread = NSThread {
+        result = predicate()
+    }
+    
+    thread.start()
+    
+    while !thread.finished {
+        NSThread.sleepForTimeInterval(0.01)
+    }
+    
+    return result
 }
