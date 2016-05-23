73c2bfe63f7ff0070b15e47552f830559445cb5e
Philippe Hausler <phausler@apple.com>
2016-05-23 09:41:58 -0700

    Update APIs for URLAuthenticationChallenge

--- Foundation/NSURLAuthenticationChallenge.swift
+++ Foundation/NSURLAuthenticationChallenge.swift
@@ -7,6 +7,38 @@
 // See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 
+public protocol URLAuthenticationChallengeSender : NSObjectProtocol {
+    
+    
+    /*!
+     @method useCredential:forAuthenticationChallenge:
+     */
+    func use(_ credential: URLCredential, for challenge: URLAuthenticationChallenge)
+    
+    
+    /*!
+     @method continueWithoutCredentialForAuthenticationChallenge:
+     */
+    func continueWithoutCredential(for challenge: URLAuthenticationChallenge)
+    
+    
+    /*!
+     @method cancelAuthenticationChallenge:
+     */
+    func cancel(_ challenge: URLAuthenticationChallenge)
+    
+    
+    /*!
+     @method performDefaultHandlingForAuthenticationChallenge:
+     */
+    func performDefaultHandling(for challenge: URLAuthenticationChallenge)
+    
+    
+    /*!
+     @method rejectProtectionSpaceAndContinueWithChallenge:
+     */
+    func rejectProtectionSpaceAndContinue(with challenge: URLAuthenticationChallenge)
+}
 
 /*!
     @class NSURLAuthenticationChallenge
@@ -28,79 +60,118 @@ public class URLAuthenticationChallenge : NSObject, NSSecureCoding {
         NSUnimplemented()
     }
     
-    /*!
-        @method initWithProtectionSpace:proposedCredential:previousFailureCount:failureResponse:error:
-        @abstract Initialize an authentication challenge 
-        @param space The NSURLProtectionSpace to use
-        @param credential The proposed NSURLCredential for this challenge, or nil
-        @param previousFailureCount A count of previous failures attempting access.
-        @param response The NSURLResponse for the authentication failure, if applicable, else nil
-        @param error The NSError for the authentication failure, if applicable, else nil
-        @result An authentication challenge initialized with the specified parameters
-    */
-    /// - Experiment: This is a draft API currently under consideration for official import into Foundation
-    /// - Note: Since this API is under consideration it may be either removed or revised in the near future
-    public init(protectionSpace space: URLProtectionSpace, proposedCredential credential: URLCredential?, previousFailureCount: Int, failureResponse response: URLResponse?, error: NSError?) { NSUnimplemented() }
     
     /*!
-        @method initWithAuthenticationChallenge:
-        @abstract Initialize an authentication challenge copying all parameters from another one.
-        @param challenge
-        @result A new challenge initialized with the parameters from the passed in challenge
-        @discussion This initializer may be useful to subclassers that want to proxy
-        one type of authentication challenge to look like another type.
-    */
-    /// - Experiment: This is a draft API currently under consideration for official import into Foundation
-    /// - Note: Since this API is under consideration it may be either removed or revised in the near future
-    public init(authenticationChallenge challenge: URLAuthenticationChallenge) { NSUnimplemented() }
+     @method initWithProtectionSpace:proposedCredential:previousFailureCount:failureResponse:error:
+     @abstract Initialize an authentication challenge
+     @param space The NSURLProtectionSpace to use
+     @param credential The proposed NSURLCredential for this challenge, or nil
+     @param previousFailureCount A count of previous failures attempting access.
+     @param response The NSURLResponse for the authentication failure, if applicable, else nil
+     @param error The NSError for the authentication failure, if applicable, else nil
+     @result An authentication challenge initialized with the specified parameters
+     */
+    public init(protectionSpace space: URLProtectionSpace, proposedCredential credential: URLCredential?, previousFailureCount: Int, failureResponse response: URLResponse?, error: NSError?, sender: URLAuthenticationChallengeSender) {
+        NSUnimplemented()
+    }
+    
     
     /*!
-        @method protectionSpace
-        @abstract Get a description of the protection space that requires authentication
-        @result The protection space that needs authentication
-    */
-    /*@NSCopying*/ public var protectionSpace: URLProtectionSpace { NSUnimplemented() }
+     @method initWithAuthenticationChallenge:
+     @abstract Initialize an authentication challenge copying all parameters from another one.
+     @param challenge
+     @result A new challenge initialized with the parameters from the passed in challenge
+     @discussion This initializer may be useful to subclassers that want to proxy
+     one type of authentication challenge to look like another type.
+     */
+    public init(authenticationChallenge challenge: URLAuthenticationChallenge, sender: URLAuthenticationChallengeSender) {
+        NSUnimplemented()
+    }
+    
     
     /*!
-        @method proposedCredential
-        @abstract Get the proposed credential for this challenge
-        @result The proposed credential
-        @discussion proposedCredential may be nil, if there is no default
-        credential to use for this challenge (either stored or in the
-        URL). If the credential is not nil and returns YES for
-        hasPassword, this means the NSURLConnection thinks the credential
-        is ready to use as-is. If it returns NO for hasPassword, then the
-        credential is not ready to use as-is, but provides a default
-        username the client could use when prompting.
-    */
-    /*@NSCopying*/ public var proposedCredential: URLCredential? { NSUnimplemented() }
+     @method protectionSpace
+     @abstract Get a description of the protection space that requires authentication
+     @result The protection space that needs authentication
+     */
+    /*@NSCopying*/ public var protectionSpace: URLProtectionSpace {
+        get {
+            NSUnimplemented()
+        }
+    }
+    
     
     /*!
-        @method previousFailureCount
-        @abstract Get count of previous failed authentication attempts
-        @result The count of previous failures
-    */
-    public var previousFailureCount: Int { NSUnimplemented() }
+     @method proposedCredential
+     @abstract Get the proposed credential for this challenge
+     @result The proposed credential
+     @discussion proposedCredential may be nil, if there is no default
+     credential to use for this challenge (either stored or in the
+     URL). If the credential is not nil and returns YES for
+     hasPassword, this means the NSURLConnection thinks the credential
+     is ready to use as-is. If it returns NO for hasPassword, then the
+     credential is not ready to use as-is, but provides a default
+     username the client could use when prompting.
+     */
+    /*@NSCopying*/ public var proposedCredential: URLCredential? {
+        get {
+            NSUnimplemented()
+        }
+    }
+    
     
     /*!
-        @method failureResponse
-        @abstract Get the response representing authentication failure.
-        @result The failure response or nil
-        @discussion If there was a previous authentication failure, and
-        this protocol uses responses to indicate authentication failure,
-        then this method will return the response. Otherwise it will
-        return nil.
-    */
-    /*@NSCopying*/ public var failureResponse: URLResponse? { NSUnimplemented() }
+     @method previousFailureCount
+     @abstract Get count of previous failed authentication attempts
+     @result The count of previous failures
+     */
+    public var previousFailureCount: Int {
+        get {
+            NSUnimplemented()
+        }
+    }
+    
     
     /*!
-        @method error
-        @abstract Get the error representing authentication failure.
-        @discussion If there was a previous authentication failure, and
-        this protocol uses errors to indicate authentication failure,
-        then this method will return the error. Otherwise it will
-        return nil.
-    */
-    /*@NSCopying*/ public var error: NSError? { NSUnimplemented() }
+     @method failureResponse
+     @abstract Get the response representing authentication failure.
+     @result The failure response or nil
+     @discussion If there was a previous authentication failure, and
+     this protocol uses responses to indicate authentication failure,
+     then this method will return the response. Otherwise it will
+     return nil.
+     */
+    /*@NSCopying*/ public var failureResponse: URLResponse? {
+        get {
+            NSUnimplemented()
+        }
+    }
+    
+    
+    /*!
+     @method error
+     @abstract Get the error representing authentication failure.
+     @discussion If there was a previous authentication failure, and
+     this protocol uses errors to indicate authentication failure,
+     then this method will return the error. Otherwise it will
+     return nil.
+     */
+    /*@NSCopying*/ public var error: NSError? {
+        get {
+            NSUnimplemented()
+        }
+    }
+    
+    
+    /*!
+     @method sender
+     @abstract Get the sender of this challenge
+     @result The sender of the challenge
+     @discussion The sender is the object you should reply to when done processing the challenge.
+     */
+    public var sender: URLAuthenticationChallengeSender? {
+        get {
+            NSUnimplemented()
+        }
+    }
 }
-
