539d8d535417b10fce368ed7b20da7a4d8111df2
Philippe Hausler <phausler@apple.com>
2016-05-22 22:59:54 -0700

    Drop NS from LengthFormatter

--- Foundation/NSLengthFormatter.swift
+++ Foundation/NSLengthFormatter.swift
@@ -8,19 +8,21 @@
 //
 
 
-public enum NSLengthFormatterUnit : Int {
-    
-    case millimeter
-    case centimeter
-    case meter
-    case kilometer
-    case inch
-    case foot
-    case yard
-    case mile
+extension LengthFormatter {
+    public enum Unit : Int {
+        
+        case millimeter
+        case centimeter
+        case meter
+        case kilometer
+        case inch
+        case foot
+        case yard
+        case mile
+    }
 }
 
-public class NSLengthFormatter : NSFormatter {
+public class LengthFormatter : NSFormatter {
     
     public required init?(coder: NSCoder) {
         NSUnimplemented()
@@ -32,16 +34,16 @@ public class NSLengthFormatter : NSFormatter {
     public var forPersonHeightUse: Bool // default is NO; if it is set to YES, the number argument for -stringFromMeters: and -unitStringFromMeters: is considered as a person's height
     
     // Format a combination of a number and an unit to a localized string.
-    public func stringFromValue(_ value: Double, unit: NSLengthFormatterUnit) -> String { NSUnimplemented() }
+    public func stringFromValue(_ value: Double, unit: Unit) -> String { NSUnimplemented() }
     
     // Format a number in meters to a localized string with the locale-appropriate unit and an appropriate scale (e.g. 4.3m = 14.1ft in the US locale).
     public func stringFromMeters(_ numberInMeters: Double) -> String { NSUnimplemented() }
     
     // Return a localized string of the given unit, and if the unit is singular or plural is based on the given number.
-    public func unitStringFromValue(_ value: Double, unit: NSLengthFormatterUnit) -> String { NSUnimplemented() }
+    public func unitStringFromValue(_ value: Double, unit: Unit) -> String { NSUnimplemented() }
     
     // Return the locale-appropriate unit, the same unit used by -stringFromMeters:.
-    public func unitStringFromMeters(_ numberInMeters: Double, usedUnit unitp: UnsafeMutablePointer<NSLengthFormatterUnit>) -> String { NSUnimplemented() }
+    public func unitStringFromMeters(_ numberInMeters: Double, usedUnit unitp: UnsafeMutablePointer<Unit>) -> String { NSUnimplemented() }
     
     /// - Experiment: This is a draft API currently under consideration for official import into Foundation as a suitable alternative
     /// - Note: Since this API is under consideration it may be either removed or revised in the near future
