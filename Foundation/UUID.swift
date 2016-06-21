3484fad7097743f66fe2c8658c248d7855b91425
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-21 00:26:01 -0500

    Fixed copyright notice for UUID

--- Foundation/UUID.swift
+++ Foundation/UUID.swift
@@ -1,10 +1,12 @@
+// This source file is part of the Swift.org open source project
 //
-//  UUID.swift
-//  Foundation
+// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
-//  Created by Alsey Coleman Miller on 6/20/16.
-//  Copyright © 2016 Apple. All rights reserved.
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
+
 
 import CoreFoundation
 
