a1906c31b02ffba1f7f2bcc7781ca8bb892b1b1c
Philippe Hausler <phausler@apple.com>
2016-06-23 15:01:24 -0700

    re-enable tests in NSURLResponse

--- Foundation/NSURLResponse.swift
+++ Foundation/NSURLResponse.swift
@@ -258,7 +258,7 @@ private func getSuggestedFilename(fromHeaderFields headerFields: [String : Strin
         else { return nil }
     for part in field.parameters where part.attribute == "filename" {
         if let path = part.value {
-            return URL(fileURLWithPath: path).pathComponents!.map{ $0 == "/" ? "" : $0}.joined(separator: "_")
+            return _pathComponents(path)?.map{ $0 == "/" ? "" : $0}.joined(separator: "_")
         } else {
             return nil
         }
