35c3b626527193e379a5e4090b1a9e68a97c1393
DYL <dyl@davids-Mac-Pro.local>
2016-07-25 18:35:24 -0700

    swift header docs

--- Foundation/OutStream.swift
+++ Foundation/OutStream.swift
@@ -15,18 +15,27 @@ import CoreFoundation
 public struct OutStream: ReferenceConvertible, CustomStringConvertible, Equatable, Hashable,_MutablePairBoxing{
     public typealias ReferenceType = NSOutputStream
     public typealias Status = NSStream.Status
+ 
     
-    //MARK: - Stored Properties
+//MARK: - Stored Properties
     internal var _wrapped : _SwiftOutputStream
     
-    //MARK: - Computed Properties
-    public var description: String { return _mapUnmanaged{ $0.description } }
+//MARK: - Computed Properties
+    public var description: String {
+        return _mapUnmanaged{ $0.description }
+    }
     
-    public var debugDescription: String { return _mapUnmanaged{ $0.debugDescription } }
+    public var debugDescription: String {
+        return _mapUnmanaged{ $0.debugDescription }
+    }
     
-    public var hashValue: Int { return _mapUnmanaged{ $0.hashValue } }
+    public var hashValue: Int {
+        return _mapUnmanaged{ $0.hashValue }
+    }
     
-    public var streamError:NSError? { return _wrapped._mapUnmanaged{ $0.streamError } }
+    public var streamError:NSError? {
+        return _wrapped._mapUnmanaged{ $0.streamError }
+    }
     
     public var hasSpaceAvailable: Bool {
         return _wrapped._mapUnmanaged{ $0.hasSpaceAvailable }
@@ -36,7 +45,7 @@ public struct OutStream: ReferenceConvertible, CustomStringConvertible, Equatabl
         return _wrapped._mapUnmanaged{ $0.streamStatus }
     }
     
-    //MARK: - Initialization
+//MARK: - Initialization
     public init(toMemory: ()) {
         _wrapped = _SwiftOutputStream(immutableObject: NSOutputStream(toMemory: toMemory))
     }
@@ -60,6 +69,12 @@ public struct OutStream: ReferenceConvertible, CustomStringConvertible, Equatabl
     }
 
 //MARK: - Functions
+    /**
+     open: A stream must be created before it can be opened. Once opened, a stream cannot be closed and reopened.
+     ```swift
+     let didOpen = outStream.open()
+     ```
+    */
     public func open() {
         _wrapped._mapUnmanaged{ $0.open() }
     }
@@ -68,22 +83,74 @@ public struct OutStream: ReferenceConvertible, CustomStringConvertible, Equatabl
         _wrapped._mapUnmanaged{ $0.close() }
     }
    
+    /**
+     propertyForKey: get Stream's configuration for a key
+     ```swift
+        let val = outStream.propertyForKey(akey)
+     ```
+     - Parameter key: The key for one of the receiver's properties.
+     
+     - SeeAlso: Constants for a description of the available property-key constants and associated values.
+     
+
+     - Returns: The receiver’s property for the key key.
+     */
     public func propertyForKey(_ key: String) -> AnyObject? {
         return _wrapped._mapUnmanaged{ $0.propertyForKey(key) }
     }
     
+    /**
+     setProperty: Attempts to set the value of a given property of the receiver and returns a Boolean value that indicates whether the value is accepted by the receiver.
+     ```swift
+        outStream.setProperty(value, forKey:aValidKey)
+     ```
+     - Parameter property: The value for key.
+     - Parameter key: The key for one of the receiver's properties.
+     
+    
+     - SeeAlso: Constants for a description of the available property-key constants and associated values.
+     
+     
+     - Returns: The receiver’s property for the key key.
+     */
     public mutating func setProperty(_ property: AnyObject?, forKey key: String) -> Bool {
         return _applyUnmanagedMutation{
             return $0.setProperty(property, forKey: key)
         }
     }
     
+    /**
+     write: Writes the contents of a provided data buffer to the receiver.
+     ```swift
+     outStream.write(encodedData, maxLength: encodedData.count)
+     ```
+     - Parameter buffer: The data to write.
+     - Parameter length: The length of the data buffer, in bytes.
+     
+
+     - Note: The behavior of this method is undefined if you pass a negative or zero number as length.
+     
+     Returns
+        - A positive number indicates the number of bytes written.
+        - 0 indicates that a fixed-length stream and has reached its capacity.
+        - -1 means that the operation failed; more information about the error can be obtained with streamError.
+     */
     public func write(_ buffer: UnsafePointer<UInt8>, maxLength len: Int) -> Int {
         return _mapUnmanaged{
             return $0.write(buffer, maxLength: len)
         }
     }
-
+    /**
+     outputStreamToMemory: Creates and returns an initialized output stream that will write stream data to memory.
+     The stream must be opened before it can be used.
+     
+     You retrieve the contents of the memory stream by sending the message propertyForKey: to the receiver with an argument of NSStreamDataWrittenToMemoryStreamKey.
+     ```swift
+        OutStream.outputStreamToMemory()
+     ```
+     
+     - Returns: An initialized output stream that will write stream data to memory.
+     */
     public static func outputStreamToMemory() -> OutStream {
         return OutStream(toMemory: ())
     }
@@ -115,6 +182,7 @@ extension OutStream {
     public static func _forceBridgeFromObjectiveC(_ input: NSOutputStream, result: inout OutStream?) {
         result = OutStream(_bridged: input)
     }
+    
     public static func _conditionallyBridgeFromObjectiveC(_ input: NSOutputStream, result: inout OutStream?) -> Bool {
         result = OutStream(_bridged: input)
         return true
