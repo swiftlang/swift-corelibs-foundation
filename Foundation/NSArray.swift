c05c3547bf72e3cdfa8acc0b4b7798ddc081a218
Jordan Rose <jordan_rose@apple.com>
2016-08-01 19:45:24 -0700

    Revert "Updates for SE-0110 Part I" (#500)
    The changes to higher-order functions are not correct.

--- Foundation/NSArray.swift
+++ Foundation/NSArray.swift
@@ -384,7 +384,7 @@ open class NSArray : NSObject, NSCopying, NSMutableCopying, NSSecureCoding, NSCo
             let hash = item.hash
             buffer.advanced(by: idx).pointee = Int32(hash).littleEndian
         }
-        return Data(bytesNoCopy: unsafeBitCast(buffer, to: UnsafeMutablePointer<UInt8>.self), count: count * MemoryLayout<Int>.size, deallocator: .custom({ _, _ in
+        return Data(bytesNoCopy: unsafeBitCast(buffer, to: UnsafeMutablePointer<UInt8>.self), count: count * MemoryLayout<Int>.size, deallocator: .custom({ _ in
             buffer.deallocate(capacity: size)
             buffer.deinitialize(count: size)
         }))
