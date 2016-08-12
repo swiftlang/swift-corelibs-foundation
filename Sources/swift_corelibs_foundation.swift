ae719dd30df024d02118f8c70c4b38e3be511cda
Amr Aboelela <amraboelela@gmail.com>
2016-08-12 12:22:32 -0700

    Added files after running "swift package init"

---
+++ Sources/swift_corelibs_foundation.swift
@@ -0,0 +1,4 @@
+struct swift_corelibs_foundation {
+
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import swift_corelibs_foundationTests
+
+XCTMain([
+     testCase(swift_corelibs_foundationTests.allTests),
+])
