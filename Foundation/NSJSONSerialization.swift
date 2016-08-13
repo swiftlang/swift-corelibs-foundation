2ca99ff049e66b46f8d0c25cd2d8ad32377aada6
david liu <david.yang.liu@gmail.com>
2016-08-13 13:19:42 -0700

    [SR-2151]NSJSONSerialization.data produces illegal JSON code
    NSJSONSerialization.data(withJSONObject:options) produces illegal JSON code
    
    https://bugs.swift.org/browse/SR-2151

--- Foundation/NSJSONSerialization.swift
+++ Foundation/NSJSONSerialization.swift
@@ -318,7 +318,7 @@ private struct JSONWriter {
         // Cannot detect type information (e.g. bool) as there is no objCType property on NSNumber in Swift
         // So, just print the number
 
-        writer("\(num)")
+        writer(num.serializationString)
     }
 
     mutating func serializeArray(_ array: [Any]) throws {
