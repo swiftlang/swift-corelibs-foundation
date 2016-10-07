38b9327df321ed1fcb93f769887deac0620f8e80
Vivian Kong <vivkong@ca.ibm.com>
2016-10-07 14:34:09 -0400

    missing -lcurl for s390x

--- build.py
+++ build.py
@@ -81,7 +81,7 @@ if "XCTEST_BUILD_DIR" in Configuration.current.variables:
 	]
 
 triple = Configuration.current.target.triple
-if triple == "x86_64-linux-gnu" or triple == "armv7-none-linux-androideabi":
+if triple == "x86_64-linux-gnu" or triple == "armv7-none-linux-androideabi" or triple == "s390x-linux-gnu":
 	foundation.LDFLAGS += '-lcurl '
 
 if triple == "armv7-none-linux-androideabi":
