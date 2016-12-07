83d2c3c51fc9873196006125f91552ca97ab8bbb
Philippe Hausler <phausler@apple.com>
2016-12-07 15:14:59 -0800

    remove extra implicit bridging gates for CFPropertyList.h

--- CoreFoundation/Parsing.subproj/CFPropertyList.h
+++ CoreFoundation/Parsing.subproj/CFPropertyList.h
@@ -25,8 +25,6 @@ typedef CF_OPTIONS(CFOptionFlags, CFPropertyListMutabilityOptions) {
     kCFPropertyListMutableContainersAndLeaves
 };
 
-CF_IMPLICIT_BRIDGING_DISABLED
-
 /*
 	Creates a property list object from its XML description; xmlData should
 	be the raw bytes of that description, possibly the contents of an XML
@@ -55,8 +53,6 @@ CFPropertyListRef CFPropertyListCreateFromXMLData(CFAllocatorRef allocator, CFDa
 CF_EXPORT
 CFDataRef CFPropertyListCreateXMLData(CFAllocatorRef allocator, CFPropertyListRef propertyList) CF_DEPRECATED(10_0, 10_10, 2_0, 8_0, "Use CFPropertyListCreateData instead.");
 
-CF_IMPLICIT_BRIDGING_ENABLED
-
 /*
 	Recursively creates a copy of the given property list (so nested arrays
 	and dictionaries are copied as well as the top-most container). The
@@ -79,8 +75,6 @@ typedef CF_ENUM(CFIndex, CFPropertyListFormat) {
  * the format parameter. */
 CF_EXPORT
 Boolean CFPropertyListIsValid(CFPropertyListRef plist, CFPropertyListFormat format);
-
-CF_IMPLICIT_BRIDGING_DISABLED
 
 /* Writes the bytes of a plist serialization out to the stream.  The
  * stream must be opened and configured -- the function simply writes
@@ -110,10 +104,6 @@ CFIndex CFPropertyListWriteToStream(CFPropertyListRef propertyList, CFWriteStrea
 CF_EXPORT
 CFPropertyListRef CFPropertyListCreateFromStream(CFAllocatorRef allocator, CFReadStreamRef stream, CFIndex streamLength, CFOptionFlags mutabilityOption, CFPropertyListFormat *format, CFStringRef *errorString) CF_DEPRECATED(10_2, 10_10, 2_0, 8_0, "Use CFPropertyListCreateWithStream instead.");
 
-CF_IMPLICIT_BRIDGING_ENABLED
-
-CF_IMPLICIT_BRIDGING_DISABLED
-
 CF_ENUM(CFIndex) {
     kCFPropertyListReadCorruptError = 3840,              // Error parsing a property list
     kCFPropertyListReadUnknownVersionError = 3841,       // The version number in the property list is unknown
@@ -140,8 +130,6 @@ CFIndex CFPropertyListWrite(CFPropertyListRef propertyList, CFWriteStreamRef str
  */
 CF_EXPORT
 CFDataRef CFPropertyListCreateData(CFAllocatorRef allocator, CFPropertyListRef propertyList, CFPropertyListFormat format, CFOptionFlags options, CFErrorRef *error) CF_AVAILABLE(10_6, 4_0);
-
-CF_IMPLICIT_BRIDGING_ENABLED
 
 CF_EXTERN_C_END
 CF_IMPLICIT_BRIDGING_DISABLED
