33c6632fc92480f8a9d1284f7dc27f0ffbfe9010
Sergey Minakov <devias.naith@gmail.com>
2017-02-09 18:15:20 +0400

    Update TestNSCache.swift

--- TestFoundation/TestNSCache.swift
+++ TestFoundation/TestNSCache.swift
@@ -1,9 +1,10 @@
+// This source file is part of the Swift.org open source project
 //
-//  TestNSCache.swift
-//  Foundation
+// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
-//  Created by Sergey Minakov on 09.02.17.
-//  Copyright © 2017 Apple. All rights reserved.
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 
 #if DEPLOYMENT_RUNTIME_OBJC || os(Linux)
