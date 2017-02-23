7726f57110bc8c37fe7cecab778e8af2b2f8cc64
David Dunn <DavDunn2@uk.ibm.com>
2017-02-23 16:08:13 +0000

    Initial implementation of ByteCountFormatter

--- Foundation/NSByteCountFormatter.swift
+++ Foundation/NSByteCountFormatter.swift
@@ -47,48 +47,261 @@ open class ByteCountFormatter : Formatter {
     }
     
     public required init?(coder: NSCoder) {
-        NSUnimplemented()
+        super.init(coder: coder)
     }
     
-    /* Shortcut for converting a byte count into a string without creating an NSByteCountFormatter and an NSNumber. If you need to specify options other than countStyle, create an instance of NSByteCountFormatter first.
-    */
-    open class func string(fromByteCount byteCount: Int64, countStyle: ByteCountFormatter.CountStyle) -> String { NSUnimplemented() }
-    
-    /* Convenience method on string(for:):. Convert a byte count into a string without creating an NSNumber.
-    */
-    open func string(fromByteCount byteCount: Int64) -> String { NSUnimplemented() }
-    
     /* Specify the units that can be used in the output. If NSByteCountFormatterUseDefault, uses platform-appropriate settings; otherwise will only use the specified units. This is the default value. Note that ZB and YB cannot be covered by the range of possible values, but you can still choose to use these units to get fractional display ("0.0035 ZB" for instance).
-    */
+     */
     open var allowedUnits: Units = .useDefault
     
     /* Specify how the count is displayed by indicating the number of bytes to be used for kilobyte. The default setting is NSByteCountFormatterFileCount, which is the system specific value for file and storage sizes.
-    */
+     */
     open var countStyle: CountStyle = .file
     
     /* Choose whether to allow more natural display of some values, such as zero, where it may be displayed as "Zero KB," ignoring all other flags or options (with the exception of NSByteCountFormatterUseBytes, which would generate "Zero bytes"). The result is appropriate for standalone output. Default value is YES. Special handling of certain values such as zero is especially important in some languages, so it's highly recommended that this property be left in its default state.
-    */
+     */
     open var allowsNonnumericFormatting: Bool = true
     
     /* Choose whether to include the number or the units in the resulting formatted string. (For example, instead of 723 KB, returns "723" or "KB".) You can call the API twice to get both parts, separately. But note that putting them together yourself via string concatenation may be wrong for some locales; so use this functionality with care.  Both of these values are YES by default.  Setting both to NO will unsurprisingly result in an empty string.
-    */
+     */
     open var includesUnit: Bool = true
     open var includesCount: Bool = true
     
     /* Choose whether to parenthetically (localized as appropriate) display the actual number of bytes as well, for instance "723 KB (722,842 bytes)".  This will happen only if needed, that is, the first part is already not showing the exact byte count.  If includesUnit or includesCount are NO, then this setting has no effect.  Default value is NO.
-    */
+     */
     open var includesActualByteCount: Bool = false
     
     /* Choose the display style. The "adaptive" algorithm is platform specific and uses a different number of fraction digits based on the magnitude (in 10.8: 0 fraction digits for bytes and KB; 1 fraction digits for MB; 2 for GB and above). Otherwise the result always tries to show at least three significant digits, introducing fraction digits as necessary. Default is YES.
-    */
+     */
     open var isAdaptive: Bool = true
     
     /* Choose whether to zero pad fraction digits so a consistent number of fraction digits are displayed, causing updating displays to remain more stable. For instance, if the adaptive algorithm is used, this option formats 1.19 and 1.2 GB as "1.19 GB" and "1.20 GB" respectively, while without the option the latter would be displayed as "1.2 GB". Default value is NO.
-    */
+     */
     open var zeroPadsFractionDigits: Bool = false
     
     /* Specify the formatting context for the formatted string. Default is NSFormattingContextUnknown.
-    */
+     */
     open var formattingContext: Context = .unknown
-}
+
+    /* A variable to store the actual bytes passed into the methods. This value is used if the includesActualByteCount property is set.
+    */
+    private var actualBytes: String = ""
+    
+    /* Create an instance of NumberFormatter for use in various methods
+    */
+    private let numberFormatter = NumberFormatter()
+    
+    /* Shortcut for converting a byte count into a string without creating an NSByteCountFormatter and an NSNumber. If you need to specify options other than countStyle, create an instance of NSByteCountFormatter first.
+    */
+    open class func string(fromByteCount byteCount: Int64, countStyle: ByteCountFormatter.CountStyle) -> String {
+        let formatter = ByteCountFormatter()
+        formatter.countStyle = countStyle
+        return formatter.string(fromByteCount: byteCount)
+    }
+    
+    /* Convenience method on string(for:):. Convert a byte count into a string without creating an NSNumber.
+    */
+    open func string(fromByteCount byteCount: Int64) -> String {
+        //Convert actual bytes to a formatted string for use later
+        if includesActualByteCount {
+            numberFormatter.numberStyle = .decimal
+            actualBytes = numberFormatter.string(from: NSNumber(value: byteCount))!
+        }
+        
+        if allowedUnits != .useDefault && allowedUnits != .useAll {
+            if countStyle == .file || countStyle == .decimal {
+                return unitsToUseFor(byteCount: byteCount, byteSize: decimalByteSize)
+            } else {
+                return unitsToUseFor(byteCount: byteCount, byteSize: binaryByteSize)
+            }
+        } else if countStyle == .decimal || countStyle == .file {
+            return convertValue(fromByteCount: byteCount, for: decimalByteSize)
+        } else {
+            return convertValue(fromByteCount: byteCount, for: binaryByteSize)
+        }
+    }
+    
+    /* Convenience method on string(for:):. Convert a byte count into a string without creating an NSNumber.
+     */
+    open override func string(for obj: Any?) -> String? {
+        guard let value = obj as? Double else {
+            return nil
+        }
+        
+        return string(fromByteCount: Int64(value))
+    }
+    
+    /* If allowedUnits has been set this function will ensure the correct unit is used and conversion is done. The conversion is done by making use of the divide method.
+    */
+    private func unitsToUseFor(byteCount: Int64, byteSize: [Unit: Double]) -> String {
+        let bytes = Double(byteCount)
+        
+        if bytes == 0 {
+            return "Zero \(Unit.KB)"
+        } else if bytes == 1 {
+            return formatNumberFor(bytes: bytes, unit: Unit.byte)
+        }
+        
+        switch allowedUnits {
+        case Units.useBytes: return formatNumberFor(bytes: bytes, unit: Unit.bytes)
+        case Units.useKB: return divide(bytes, by: byteSize, for: .KB)
+        case Units.useMB: return divide(bytes, by: byteSize, for: .MB)
+        case Units.useGB: return divide(bytes, by: byteSize, for: .GB)
+        case Units.useTB: return divide(bytes, by: byteSize, for: .TB)
+        case Units.usePB: return divide(bytes, by: byteSize, for: .PB)
+        case Units.useEB: return divide(bytes, by: byteSize, for: .EB)
+        case Units.useZB: return divide(bytes, by: byteSize, for: .ZB)
+        default: return divide(bytes, by: byteSize, for: .YB)
+            
+        }
+    }
+    
+    /* This method accepts a byteCount and a byteSize value. Checks to see what range the byteCount falls into and then converts to the units determined by that range. The range to be used is decided by the byteSize parameter. The conversion is done by making use of the divide method.
+    */
+    private func convertValue(fromByteCount byteCount: Int64, for byteSize: [Unit: Double]) -> String {
+        let byte = Double(byteCount)
+        if byte == 0 && allowsNonnumericFormatting {
+            return "Zero \(Unit.KB)"
+        } else if byte == 1 {
+            return "\(byteCount) \(Unit.byte)"
+            
+        } else if  byte < byteSize[Unit.KB]! && byte > -byteSize[Unit.KB]!{
+            return formatNumberFor(bytes: byte, unit: Unit.bytes)
+            
+        } else if byte < byteSize[Unit.MB]! && byte > -byteSize[Unit.MB]! {
+            return divide(byte, by: byteSize, for: .KB)
+            
+        } else if byte < byteSize[Unit.GB]! && byte > -byteSize[Unit.GB]! {
+            return divide(byte, by: byteSize, for: .MB)
+            
+        } else if byte < byteSize[Unit.TB]! && byte > -byteSize[Unit.TB]! {
+            return divide(byte, by: byteSize, for: .GB)
+            
+        } else if byte < byteSize[Unit.PB]! && byte > -byteSize[Unit.PB]! {
+            return divide(byte, by: byteSize, for: .TB)
+            
+        } else if byte < byteSize[Unit.EB]! && byte > -byteSize[Unit.EB]! {
+            return divide(byte, by: byteSize, for: .PB)
+            
+        } else {
+            return divide(byte, by: byteSize, for: .EB)
+        }
+    }
+    
+    // Coverts the number of bytes to the correct value given a specified unit, then passes the value and unit to formattedValue
+    private func divide(_ bytes: Double, by byteSize: [Unit: Double], for unit: Unit) -> String {
+        guard let byteSizeUnit = byteSize[unit] else {
+            fatalError("Cannot find value \(unit)")
+        }
+        let result = bytes/byteSizeUnit
+        return formatNumberFor(bytes: result, unit: unit)
+    }
+    
+    //Formats the byte value using the NumberFormatter class based on set properties and the unit passed in as a parameter.
+    private func formatNumberFor(bytes: Double, unit: Unit) -> String {
+        
+        numberFormatter.numberStyle = .decimal
+        
+        switch (zeroPadsFractionDigits, isAdaptive) {
+        //zeroPadsFractionDigits is true, isAdaptive is true
+        case (true, true):
+            switch unit {
+            case .bytes, .byte, .KB:
+                numberFormatter.minimumFractionDigits = 0
+                numberFormatter.maximumFractionDigits = 0
+                let result = numberFormatter.string(from: NSNumber(value: bytes))
+                return partsToIncludeFor(value: result!, unit: unit)
+            case .MB:
+                numberFormatter.minimumFractionDigits = 1
+                numberFormatter.maximumFractionDigits = 1
+                let result = numberFormatter.string(from: NSNumber(value: bytes))
+                return partsToIncludeFor(value: result!, unit: unit)
+            default:
+                numberFormatter.minimumFractionDigits = 2
+                numberFormatter.maximumFractionDigits = 2
+                let result = numberFormatter.string(from: NSNumber(value: bytes))
+                return partsToIncludeFor(value: result!, unit: unit)
+            }
+        //zeroPadsFractionDigits is true, isAdaptive is false
+        case (true, false):
+            if unit == .byte || unit == .bytes {
+                return partsToIncludeFor(value: "\(bytes)", unit: unit)
+            } else {
+                numberFormatter.usesSignificantDigits = true
+                numberFormatter.minimumSignificantDigits = 3
+                numberFormatter.maximumSignificantDigits = 3
+                let result = numberFormatter.string(from: NSNumber(value: bytes))
+                return partsToIncludeFor(value: result!, unit: unit)
+            }
+        //zeroPadsFractionDigits is false, isAdaptive is true
+        case (false, true):
+            switch unit {
+            case .bytes, .byte, .KB:
+                numberFormatter.minimumFractionDigits = 0
+                numberFormatter.maximumFractionDigits = 0
+                let result = numberFormatter.string(from: NSNumber(value: bytes))
+                return partsToIncludeFor(value: result!, unit: unit)
+            case .MB:
+                numberFormatter.minimumFractionDigits = 0
+                numberFormatter.maximumFractionDigits = 1
+                let result = numberFormatter.string(from: NSNumber(value: bytes))
+                return partsToIncludeFor(value: result!, unit: unit)
+            default:
+                numberFormatter.minimumFractionDigits = 0
+                numberFormatter.maximumFractionDigits = 2
+                let result = numberFormatter.string(from: NSNumber(value: bytes))
+                return partsToIncludeFor(value: result!, unit: unit)
+            }
+        //zeroPadsFractionDigits is false, isAdaptive is false
+        case (false, false):
+            if unit == .byte || unit == .bytes {
+                return partsToIncludeFor(value: "\(bytes)", unit: unit)
+            } else {
+                numberFormatter.usesSignificantDigits = true
+                numberFormatter.minimumSignificantDigits = 3
+                numberFormatter.maximumSignificantDigits = 3
+                let result = numberFormatter.string(from: NSNumber(value: bytes))
+                return partsToIncludeFor(value: result!, unit: unit)
+            }
+        }
+    }
+    
+    // Returns the correct string based on the includesValue and includesUnit properties
+    private func partsToIncludeFor(value: String, unit: Unit) -> String {
+        if includesActualByteCount, includesUnit, includesCount {
+            switch unit {
+            case .byte, .bytes: return "\(value) \(unit)"
+            default: return "\(value) \(unit) (\(actualBytes) \(Unit.bytes))"
+            }
+        } else if includesCount, includesUnit {
+            return "\(value) \(unit)"
+        } else if includesCount, !includesUnit {
+            return "\(value)"
+        } else if !includesCount, includesUnit {
+            return "\(unit)"
+        } else {
+            return ""
+        }
+    }
+    
+    //Enum containing available byte units
+    private enum Unit: String {
+        case byte
+        case bytes
+        case KB
+        case MB
+        case GB
+        case TB
+        case PB
+        case EB
+        case ZB
+        case YB
+    }
+    // Maps each unit to it's corresponding value in bytes for decimal
+    private let decimalByteSize: [Unit: Double] = [.byte: 1, .KB: 1000, .MB: pow(1000, 2), .GB: pow(1000, 3), .TB: pow(1000, 4), .PB: pow(1000, 5), .EB: pow(1000, 6), .ZB: pow(1000, 7), .YB: pow(1000, 8)]
+    
+    // Maps each unit to it's corresponding value in bytes for binary
+    private let binaryByteSize: [Unit: Double] = [.byte: 1, .KB: 1024, .MB: pow(1024, 2), .GB: pow(1024, 3), .TB: pow(1024, 4), .PB: pow(1024, 5), .EB: pow(1024, 6), .ZB: pow(1024, 7), .YB: pow(1024, 8)]
+    
+    }
 
--- TestFoundation/TestNSByteCountFormatter.swift
+++ TestFoundation/TestNSByteCountFormatter.swift
@@ -23,7 +23,13 @@ class TestNSByteCountFormatter : XCTestCase {
     
     static var allTests: [(String, (TestNSByteCountFormatter) -> () throws -> Void)] {
         return [
-            ("test_DefaultValues", test_DefaultValues)
+            ("test_DefaultValues", test_DefaultValues),
+            ("test_stringFromByteCount", test_stringFromByteCount),
+            ("test_includesCountUnit", test_includesCountUnit),
+            ("test_includesActualByteCount", test_includesActualByteCount),
+            ("test_isAdaptive", test_isAdaptive),
+            ("test_zeroPadsFractionDigits", test_zeroPadsFractionDigits),
+            ("test_allowedUnits", test_allowedUnits)
         ]
     }
     
@@ -39,4 +45,131 @@ class TestNSByteCountFormatter : XCTestCase {
         XCTAssertEqual(formatter.zeroPadsFractionDigits, false)
         XCTAssertEqual(formatter.formattingContext, Formatter.Context.unknown)
     }
+    
+    func test_stringFromByteCount() {
+        let formatter = ByteCountFormatter()
+        //Default values tests
+        XCTAssertEqual(formatter.string(fromByteCount: 0), "Zero KB")
+        //Check singular value
+        XCTAssertEqual(formatter.string(fromByteCount: 1), "1 byte")
+        XCTAssertEqual(formatter.string(fromByteCount: 13), "13 bytes")
+        //Check negative value
+        XCTAssertEqual(formatter.string(fromByteCount: -13), "-13 bytes")
+        //Check rounding
+        XCTAssertEqual(formatter.string(fromByteCount: 1300), "1 KB")
+        XCTAssertEqual(formatter.string(fromByteCount: 1600), "2 KB")
+        XCTAssertEqual(formatter.string(fromByteCount: 1300000), "1.3 MB")
+        XCTAssertEqual(formatter.string(fromByteCount: 1300000000), "1.3 GB")
+        XCTAssertEqual(formatter.string(fromByteCount: 1300000000000), "1.3 TB")
+        XCTAssertEqual(formatter.string(fromByteCount: 1300000000000000), "1.3 PB")
+        XCTAssertEqual(formatter.string(fromByteCount: 1300000000000000000), "1.3 EB")
+        
+        //binary countStyle tests
+        formatter.countStyle = .binary
+        XCTAssertEqual(formatter.string(fromByteCount: 1000), "1,000 bytes")
+        XCTAssertEqual(formatter.string(fromByteCount: 1000000), "977 KB")
+        XCTAssertEqual(formatter.string(fromByteCount: 1000000000), "953.7 MB")
+        XCTAssertEqual(formatter.string(fromByteCount: 1000000000000), "931.32 GB")
+        XCTAssertEqual(formatter.string(fromByteCount: 1000000000000000), "909.49 TB")
+        XCTAssertEqual(formatter.string(fromByteCount: 1000000000000000000), "888.18 PB")
+        XCTAssertEqual(formatter.string(fromByteCount: 5000000000000000000), "4.34 EB")
+        
+        //AllowsNonnumericFormatting Test
+        formatter.allowsNonnumericFormatting = false
+        XCTAssertEqual(formatter.string(fromByteCount: 0), "0 bytes")
+        
+    }
+    
+    func test_includesCountUnit() {
+        let formatter = ByteCountFormatter()
+        formatter.includesCount = false
+        formatter.includesUnit = false
+        XCTAssertEqual(formatter.string(fromByteCount: 1300), "")
+        
+        formatter.includesUnit = true
+        XCTAssertEqual(formatter.string(fromByteCount: 1300), "KB")
+        
+        formatter.includesCount = true
+        formatter.includesUnit = false
+        XCTAssertEqual(formatter.string(fromByteCount: 1300), "1")
+    }
+    
+    func test_includesActualByteCount() {
+        let formatter = ByteCountFormatter()
+        formatter.includesActualByteCount = true
+        
+        XCTAssertEqual(formatter.string(fromByteCount: 100), "100 bytes")
+        XCTAssertEqual(formatter.string(fromByteCount: 1300), "1 KB (1,300 bytes)")
+        
+        formatter.allowedUnits = .useBytes
+        XCTAssertEqual(formatter.string(fromByteCount: 100000), "100,000 bytes")
+        formatter.allowedUnits = .useKB
+        XCTAssertEqual(formatter.string(fromByteCount: 100000), "100 KB (100,000 bytes)")
+        
+        formatter.allowedUnits = .useDefault
+        formatter.includesCount = false
+        XCTAssertEqual(formatter.string(fromByteCount: 100), "bytes")
+        XCTAssertEqual(formatter.string(fromByteCount: 1300), "KB")
+        
+        
+    }
+    
+    func test_isAdaptive() {
+        let formatter = ByteCountFormatter()
+        formatter.isAdaptive = false
+        
+        XCTAssertEqual(formatter.string(fromByteCount: 123000), "123 KB")
+        
+        //isAdaptive tests for when allowUnits is set
+        formatter.allowedUnits = .useTB
+        XCTAssertEqual(formatter.string(fromByteCount: 10000000), "0.0000100 TB")
+    }
+    
+    func test_zeroPadsFractionDigits() {
+        let formatter = ByteCountFormatter()
+        formatter.zeroPadsFractionDigits = true
+        
+        XCTAssertEqual(formatter.string(fromByteCount: 120), "120 bytes")
+        XCTAssertEqual(formatter.string(fromByteCount: 12000), "12 KB")
+        XCTAssertEqual(formatter.string(fromByteCount: 12000000), "12.0 MB")
+        XCTAssertEqual(formatter.string(fromByteCount: 12000000000), "12.00 GB")
+        
+        formatter.isAdaptive = false
+        XCTAssertEqual(formatter.string(fromByteCount: 1200), "1.20 KB")
+        XCTAssertEqual(formatter.string(fromByteCount: 12000), "12.0 KB")
+        XCTAssertEqual(formatter.string(fromByteCount: 12000000), "12.0 MB")
+        XCTAssertEqual(formatter.string(fromByteCount: 12000000000), "12.0 GB")
+    }
+    
+    func test_allowedUnits() {
+        let formatter = ByteCountFormatter()
+        formatter.allowedUnits = .useBytes
+        XCTAssertEqual(formatter.string(fromByteCount: 12000000000000), "12,000,000,000,000 bytes")
+        
+        formatter.allowedUnits = .useKB
+        XCTAssertEqual(formatter.string(fromByteCount: 12000000000000), "12,000,000,000 KB")
+        
+        formatter.allowedUnits = .useMB
+        XCTAssertEqual(formatter.string(fromByteCount: 12000000000000), "12,000,000 MB")
+        
+        formatter.allowedUnits = .useGB
+        XCTAssertEqual(formatter.string(fromByteCount: 12000000000000), "12,000 GB")
+        
+        formatter.allowedUnits = .useTB
+        XCTAssertEqual(formatter.string(fromByteCount: 12000000000000), "12 TB")
+        
+        formatter.allowedUnits = .usePB
+        XCTAssertEqual(formatter.string(fromByteCount: 12000000000000), "0.01 PB")
+        
+        formatter.allowedUnits = .useEB
+        XCTAssertEqual(formatter.string(fromByteCount: 1900000000000000000), "1.9 EB")
+        
+        formatter.allowedUnits = .useZB
+        XCTAssertEqual(formatter.string(fromByteCount: 6000000000000000000), "0.01 ZB")
+        
+        formatter.allowedUnits = .useYBOrHigher
+        XCTAssertEqual(formatter.string(fromByteCount: 6000000000000000000), "0 YB")
+        XCTAssertEqual(formatter.string(fromByteCount: 0), "Zero KB")
+    }
+
 }
