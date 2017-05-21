25f47efa8a6c7710137b448c7b87685d1a33f78a
David Dunn <ddunnibm@gmail.com>
2017-05-21 18:19:12 +0100

    Implementation of ISO8601DateFormatter

--- TestFoundation/main.swift
+++ TestFoundation/main.swift
@@ -43,6 +43,7 @@ XCTMain([
     testCase(TestNSHTTPCookieStorage.allTests),
     testCase(TestNSIndexPath.allTests),
     testCase(TestNSIndexSet.allTests),
+    testCase(TestISO8601DateFormatter.allTests),
     testCase(TestNSJSONSerialization.allTests),
     testCase(TestNSKeyedArchiver.allTests),
     testCase(TestNSKeyedUnarchiver.allTests),
@@ -95,5 +96,4 @@ XCTMain([
     testCase(TestProgress.allTests),
     testCase(TestObjCRuntime.allTests),
     testCase(TestNotification.allTests),
-    testCase(TestNSISO8601DateFormatter.allTests),
 ])
