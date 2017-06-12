b16a89c8c7317c58a01ce9e0ad45797aaff6c658
Simon Evans <si@si.org>
2017-06-12 23:56:14 +0100

    build: Fix TestFoundation to build and run in Xcode
    - The paths to the built frameworks needed updating so that
      TestFoundation will now correctly build in Xcode and link
      with XCTest.
    
    - Use the correct name to the xdgTestHelper which is different on
      Linux and macOS.
    
    - The test HTTP server now listens on 127.0.0.1 instead of 0.0.0.0
      so that the firewall dialog doesnt popup on macOS if the firewall
      is enabled.

--- Foundation/Process.swift
+++ Foundation/Process.swift
@@ -241,7 +241,7 @@ open class Process: NSObject {
         if let env = environment {
             let nenv = env.count
             envp = UnsafeMutablePointer<UnsafeMutablePointer<Int8>?>.allocate(capacity: 1 + nenv)
-            envp.initialize(from: env.map { strdup("\($0)=\($1)") }, count: 1 + nenv)
+            envp.initialize(from: env.map { strdup("\($0)=\($1)") }, count: nenv)
             envp[env.count] = nil
         } else {
             envp = _CFEnviron()
