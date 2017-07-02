bb182da3186068923da507ed0c98417232dd53b7
Norio Nomura <norio.nomura@gmail.com>
2017-07-03 08:33:02 +0900

    Remove stored instance properties from `NSNumber`

--- Foundation/NSNumber.swift
+++ Foundation/NSNumber.swift
@@ -205,9 +205,6 @@ private struct CFSInt128Struct {
 
 open class NSNumber : NSValue {
     typealias CFType = CFNumber
-    // This layout MUST be the same as CFNumber so that they are bridgeable
-    private var _base = _CFInfo(typeID: CFNumberGetTypeID())
-    private var _pad: UInt64 = 0
 
     internal var _cfObject: CFType {
         return unsafeBitCast(self, to: CFType.self)
