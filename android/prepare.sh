49c1dad1929054715a08394e199ddc34641cfb8a
Amr Aboelela <amraboelela@gmail.com>
2017-09-08 16:46:16 -0700

    Android: Fixed prepare.sh to download the toolchain instead from http://johnholdsworth.com/android_toolchain.tgz as the old link is not working anymore.

--- android/prepare.sh
+++ android/prepare.sh
@@ -6,12 +6,17 @@
 BUILD_DIR="build/Ninja-ReleaseAssert/swift-linux-x86_64/lib/swift"
 
 cd "$(dirname $0)" &&
-TAR_FILE="$(pwd)/android_dispatch.tgz" &&
+TAR_FILE="$(pwd)/android_toolchain.tgz" &&
 
 if [[ ! -f "${TAR_FILE}" ]]; then
-    echo "Fetching binaries and headers for libxml2, libcurl and libdispatch"
-    curl "https://raw.githubusercontent.com/SwiftJava/SwiftJava/master/android_dispatch.tgz" > "${TAR_FILE}"
+echo "Fetching binaries and headers for libxml2, libcurl and libdispatch"
+curl http://johnholdsworth.com/android_toolchain.tgz > "${TAR_FILE}"
 fi
 
-cd "../.." && mkdir -p "${BUILD_DIR}" &&
-cd "${BUILD_DIR}" && tar xfvz "${TAR_FILE}"
+tar xfvz "${TAR_FILE}"
+
+mkdir -p "../../${BUILD_DIR}/android/armv7" &&
+
+cp -r swift-install/usr/lib/swift/android/{libxml2.so,libcurl.so,libdispatch.so} ../../${BUILD_DIR}/android
+cp -r swift-install/usr/lib/swift/android/armv7/Dispatch.swiftmodule ../../${BUILD_DIR}/android/armv7
+rm -rf swift-install
