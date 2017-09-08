35a309e09cb266ecb90ea8530e8b1b37b034b050
John Holdsworth <github@johnholdsworth.com>
2017-09-09 00:25:57 +0100

    Thread safety

--- Foundation/URLSession/URLSession.swift
+++ Foundation/URLSession/URLSession.swift
@@ -192,6 +192,7 @@ public let NSURLSessionTransferSizeUnknown: Int64 = -1
 open class URLSession : NSObject {
     fileprivate let _configuration: _Configuration
     fileprivate let multiHandle: _MultiHandle
+    fileprivate let taskIdentifierLock = NSLock()
     fileprivate var nextTaskIdentifier = 1
     internal let workQueue: DispatchQueue 
     /// This queue is used to make public attributes on `URLSessionTask` instances thread safe.
@@ -406,9 +407,11 @@ extension URLSession._Request {
 
 fileprivate extension URLSession {
     func createNextTaskIdentifier() -> Int {
-        let i = nextTaskIdentifier
-        nextTaskIdentifier += 1
-        return i
+        return taskIdentifierLock.synchronized {
+            let i = nextTaskIdentifier
+            nextTaskIdentifier += 1
+            return i
+        }
     }
 }
 
