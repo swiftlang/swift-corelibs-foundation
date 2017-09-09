eaa0ee90ed981a938d7ff25f706f7aa9b89d3ebf
Amr Aboelela <amraboelela@gmail.com>
2017-09-08 17:02:44 -0700

    cp -r swift-install/usr/lib/swift/{curl,dispatch,libxml} ../../${BUILD_DIR}

--- android/prepare.sh
+++ android/prepare.sh
@@ -9,8 +9,8 @@ cd "$(dirname $0)" &&
 TAR_FILE="$(pwd)/android_toolchain.tgz" &&
 
 if [[ ! -f "${TAR_FILE}" ]]; then
-echo "Fetching binaries and headers for libxml2, libcurl and libdispatch"
-curl http://johnholdsworth.com/android_toolchain.tgz > "${TAR_FILE}"
+    echo "Fetching binaries and headers for libxml2, libcurl and libdispatch"
+    curl http://johnholdsworth.com/android_toolchain.tgz > "${TAR_FILE}"
 fi
 
 tar xfvz "${TAR_FILE}"
@@ -19,4 +19,7 @@ mkdir -p "../../${BUILD_DIR}/android/armv7" &&
 
 cp -r swift-install/usr/lib/swift/android/{libxml2.so,libcurl.so,libdispatch.so} ../../${BUILD_DIR}/android
 cp -r swift-install/usr/lib/swift/android/armv7/Dispatch.swiftmodule ../../${BUILD_DIR}/android/armv7
+cp -r swift-install/usr/lib/swift/{curl,dispatch,libxml} ../../${BUILD_DIR}
+
 rm -rf swift-install
+
