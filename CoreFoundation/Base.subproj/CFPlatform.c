ba09c4f93db19f669fed5ac71cf06a04cdc887a6
Lily Vulcano <millenomi@apple.com>
2018-09-28 14:20:47 -0700

    Logical, not bitwise here.

--- CoreFoundation/Base.subproj/CFPlatform.c
+++ CoreFoundation/Base.subproj/CFPlatform.c
@@ -1383,7 +1383,7 @@ int _CFOpenFile(const char *path, int opts) {
 }
 
 CF_CROSS_PLATFORM_EXPORT void *_CFReallocf(void *ptr, size_t size) {
-#if TARGET_OS_WIN32 | DEPLOYMENT_TARGET_LINUX
+#if TARGET_OS_WIN32 || DEPLOYMENT_TARGET_LINUX
     void *mem = realloc(ptr, size);
     if (mem == NULL && ptr != NULL && size != 0) {
         free(ptr);
