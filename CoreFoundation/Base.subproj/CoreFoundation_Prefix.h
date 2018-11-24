55df0070136a1af95a9b9e325f52a5f7fb05bce9
Saleem Abdulrasool <compnerd@compnerd.org>
2018-11-23 16:26:29 -0800

    base: require Windows 7 as a minimum version
    Bump up the minimum Windows requirement from Windows XP to Windows 7.
    Microsoft has EOL'ed Windows XP and Windows Vista and no longer provides
    any updates for the deployments.

--- CoreFoundation/Base.subproj/CoreFoundation_Prefix.h
+++ CoreFoundation/Base.subproj/CoreFoundation_Prefix.h
@@ -304,11 +304,11 @@ CF_EXPORT int _NS_access(const char *name, int amode);
 #define WIN32_LEAN_AND_MEAN
 
 #ifndef WINVER
-#define WINVER  0x0501
+#define WINVER  0x0601
 #endif
     
 #ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0501
+#define _WIN32_WINNT 0x0601
 #endif
 
 // The order of these includes is important
