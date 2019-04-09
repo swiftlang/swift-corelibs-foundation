8bd4ffe82e1ba9897aebdac9309fbceedf1b7a12
Lily Vulcano <millenomi@apple.com>
2019-04-09 15:07:30 -0700

    Remove stray #ifdef.

--- CoreFoundation/PlugIn.subproj/CFBundle.c
+++ CoreFoundation/PlugIn.subproj/CFBundle.c
@@ -952,11 +952,9 @@ CFURLRef _CFBundleCopyAppStoreReceiptURL(CFBundleRef bundle) {
     return _CFBundleCopyAppStoreReceiptURLInDirectory(bundle->_url, bundle->_version);
 }
 
-#ifdef CF_OPEN_SOURCE
 CF_CROSS_PLATFORM_EXPORT CFStringRef _CFBundleCopyExecutablePath(CFBundleRef bundle) {
     return _CFBundleCopyExecutableName(bundle, NULL, NULL);
 }
-#endif
 
 CF_PRIVATE CFStringRef _CFBundleCopyExecutableName(CFBundleRef bundle, CFURLRef url, CFDictionaryRef infoDict) {
     CFStringRef executableName = NULL;
