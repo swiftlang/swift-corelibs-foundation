eace6a62aa5385b12d7b8627a2594b0afce29b8d
Karoy Lorentey <klorentey@apple.com>
2019-04-12 15:58:17 -0700

    _SwiftNativeFoundationType: Add hash(into:) requirement with a default implementation

---
+++ TestFoundation/TestDateInterval.swift
@@ -0,0 +1,9 @@
+//
+//  TestDateInterval.swift
+//  TestFoundation
+//
+//  Created by Karoly Lorentey on 2019-04-10.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
---
+++ TestFoundation/TestMeasurement.swift
@@ -0,0 +1,9 @@
+//
+//  TestMeasurement.swift
+//  TestFoundation
+//
+//  Created by Karoly Lorentey on 2019-04-10.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
