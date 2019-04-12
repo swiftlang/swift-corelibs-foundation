9e376941498567e351c1af6f0b4381397d8275cf
Karoy Lorentey <klorentey@apple.com>
2019-04-12 16:16:43 -0700

    Locale: Match hash encoding with the Cocoa Foundation overlay

--- Foundation/Locale.swift
+++ Foundation/Locale.swift
@@ -423,8 +423,9 @@ public struct Locale : CustomStringConvertible, CustomDebugStringConvertible, Ha
     
     public func hash(into hasher: inout Hasher) {
         if _autoupdating {
-            hasher.combine(1 as Int8)
+            hasher.combine(false)
         } else {
+            hasher.combine(true)
             hasher.combine(_wrapped)
         }
     }
