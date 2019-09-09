ab44a0c14a5b9b80881137842129b0f2d6a724bf
Gwen Mittertreiner <gwen.mittertreiner@gmail.com>
2019-09-09 11:05:30 -0700

    Remove Verification of UTF32 XML on Windows
    Unless compiled with iconv support, libxml2 only supports
    - UTF-8
    - UTF-16, both little and big endian
    - ISO-Latin-1 (ISO-8859-1)
    - ASCII

--- TestFoundation/TestXMLParser.swift
+++ TestFoundation/TestXMLParser.swift
@@ -118,7 +118,11 @@ class TestXMLParser : XCTestCase {
         // If th <?xml header isn't present, any non-UTF8 encodings fail. This appears to be libxml2 behavior.
         // These don't work, it may just be an issue with the `encoding=xxx`.
         //   - .nextstep, .utf32LittleEndian
-        let encodings: [String.Encoding] = [.utf16LittleEndian, .utf16BigEndian, .utf32BigEndian, .ascii]
+        var encodings: [String.Encoding] = [.utf16LittleEndian, .utf16BigEndian,  .ascii]
+#if !os(Windows)
+        // libxml requires iconv support for UTF32
+        encodings.append(.utf32BigEndian)
+#endif
         for encoding in encodings {
             let xml = TestXMLParser.xmlUnderTest(encoding: encoding)
             let parser = XMLParser(data: xml.data(using: encoding)!)
