4f31458409b2149d7f82d290e2e7c6c14c625508
abdulras <compnerd@compnerd.org>
2020-02-18 18:11:24 +0000

    CoreFoundation: finish up file numeration Windows TODO
    This adds the prefix support which I ended up needing.  This achieves
    parity with the rest of the platforms.  The implementation is slightly
    different, using the `CFStringAppendBuffer` to simply reduce the
    complexity.

--- CoreFoundation/Base.subproj/CFFileUtilities.c
+++ CoreFoundation/Base.subproj/CFFileUtilities.c
@@ -1058,14 +1058,28 @@ CF_PRIVATE void _CFIterateDirectory(CFStringRef directoryPath, Boolean appendSla
                 continue;
             }
 
-            assert(!stuffToPrefix && "prefix not yet implemented");
-            Boolean isDirectory = file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY;
-            CFMutableStringRef filePath = CFStringCreateMutableCopy(kCFAllocatorSystemDefault, nameLen + 1, fileName);
-            if (appendSlashForDirectories && isDirectory) {
-                UniChar slash = CFPreferredSlash;
-                CFStringAppendCharacters(filePath, &slash, 1);
+            const UniChar kSlash = CFPreferredSlash;
+
+            CFStringAppendBuffer buffer;
+            CFStringInitAppendBuffer(kCFAllocatorSystemDefault, &buffer);
+
+            if (stuffToPrefix) {
+              for (CFIndex i = 0, e = CFArrayGetCount(stuffToPrefix); i < e; i++) {
+                CFStringRef entry = CFArrayGetValueAtIndex(stuffToPrefix, i);
+                CFStringAppendStringToAppendBuffer(&buffer, entry);
+                if (CFStringGetCharacterAtIndex(entry, CFStringGetLength(entry) - 1) != _CFGetSlash()) {
+                  CFStringAppendCharactersToAppendBuffer(&buffer, &kSlash, 1);
+                }
+              }
             }
 
+            CFStringAppendStringToAppendBuffer(&buffer, fileName);
+            Boolean isDirectory = file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY;
+            if (appendSlashForDirectories && isDirectory) {
+              CFStringAppendCharactersToAppendBuffer(&buffer, &kSlash, 1);
+            }
+
+            CFMutableStringRef filePath = CFStringCreateMutableWithAppendBuffer(&buffer);
             Boolean result = fileHandler(fileName, filePath, isDirectory ? DT_DIR : DT_REG);
             CFRelease(fileName);
             CFRelease(filePath);
