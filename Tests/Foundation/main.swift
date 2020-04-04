e6028ed50b0e205bda5d2800488d569db031fbce
Simon Evans <si@si.org>
2020-04-04 22:55:42 +0100

    Revert "TestURLSession: Re-enable tests"

--- Tests/Foundation/main.swift
+++ Tests/Foundation/main.swift
@@ -95,7 +95,7 @@ var allTestCases = [
     testCase(TestURLRequest.allTests),
     testCase(TestURLResponse.allTests),
     testCase(TestHTTPURLResponse.allTests),
-    testCase(TestURLSession.allTests),
+    testCaseExpectedToFail(TestURLSession.allTests, "URLSession test interdependencies are causing intermittent CI issues."),
     testCase(TestNSUUID.allTests),
     testCase(TestUUID.allTests),
     testCase(TestNSValue.allTests),
