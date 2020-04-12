66b92e0901c7b1fb9f043a36e4b8e8cdde15d274
Frizlab <fload@me.com>
2020-04-12 10:09:26 +0200

    Try to fix the test_preStartEndState and test_terminate tests when building Swift in a Docker
    In a Docker build context, there is no stdin, so cat exits w/ code 0 before being able to be terminated by the test program.

--- TestFoundation/TestProcess.swift
+++ TestFoundation/TestProcess.swift
@@ -373,7 +373,9 @@ class TestProcess : XCTestCase {
         XCTAssertEqual(process.processIdentifier, 0)
         XCTAssertEqual(process.qualityOfService, .default)
 
+        let pipe = Pipe()
         process.executableURL = xdgTestHelperURL()
+        process.standardInput = pipe.fileHandleForReading
         process.arguments = ["--cat"]
         _ = try? process.run()
         XCTAssertTrue(process.isRunning)
@@ -426,7 +428,12 @@ class TestProcess : XCTestCase {
     }
 
     func test_terminate() {
-        guard let process = try? Process.run(xdgTestHelperURL(), arguments: ["--cat"]) else {
+        let pipe = Pipe()
+        let process = Process()
+        process.executableURL = xdgTestHelperURL()
+        process.arguments = ["--cat"]
+        process.standardInput = pipe.fileHandleForReading
+        guard (try? process.run()) != nil else {
             XCTFail("Cant run 'cat'")
             return
         }
