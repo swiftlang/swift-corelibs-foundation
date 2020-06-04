facca683bbc273c306363e8f9073e4cc7371e9c2
Valeriy Van <github@w7software.com>
2020-06-04 11:38:54 +0200

    Eliminates redundent array creation in convinience NSString.init()

--- Sources/Foundation/NSString.swift
+++ Sources/Foundation/NSString.swift
@@ -231,8 +231,8 @@ open class NSString : NSObject, NSCopying, NSMutableCopying, NSSecureCoding, NSC
     }
     
     public override convenience init() {
-        let characters = Array<unichar>(repeating: 0, count: 1)
-        self.init(characters: characters, length: 0)
+        var nul: unichar = 0
+        self.init(characters: &nul, length: 0)
     }
     
     internal init(_ string: String) {
