3e5dbcc20ce6bd69d49e260e9f944f61f1725e36
Yuta Saito <kateinoigakukun@gmail.com>
2020-09-18 01:00:22 +0900

    [WASM] Export WASILibc

--- Sources/Foundation/NSSwiftRuntime.swift
+++ Sources/Foundation/NSSwiftRuntime.swift
@@ -14,8 +14,10 @@
 // This mimics the behavior of the swift sdk overlay on Darwin
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
 @_exported import Darwin
-#elseif os(Linux) || os(Android) || CYGWIN || os(WASI)
+#elseif os(Linux) || os(Android) || CYGWIN
 @_exported import Glibc
+#elseif os(WASI)
+@_exported import WASILibc
 #elseif os(Windows)
 @_exported import MSVCRT
 #endif
