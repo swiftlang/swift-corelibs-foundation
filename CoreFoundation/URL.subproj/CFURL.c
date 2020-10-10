7af5ec9c77ed8e4f73f949bddfe771bb95b1ca71
Valeriy Van <github@w7software.com>
2020-10-10 18:01:19 +0200

    Fix memory leak by moving CFRelease to proper place

--- CoreFoundation/URL.subproj/CFURL.c
+++ CoreFoundation/URL.subproj/CFURL.c
@@ -4815,9 +4815,9 @@ CFURLRef CFURLCreateCopyAppendingPathComponent(CFAllocatorRef allocator, CFURLRe
                 if (isDirectory) {
                     CFStringInsert(newString, pathRg.location + pathRg.length + CFStringGetLength(newComp), CFSTR("/"));
                 }
-                CFRelease(newComp);
                 result = _CFURLCreateWithArbitraryString(allocator, newString, url->_base);
             }
+            if (newComp) CFRelease(newComp);
             CFRelease(newString);
         }
     }
