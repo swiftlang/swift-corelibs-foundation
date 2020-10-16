f12636f7c731f0e3f8e3552c9ae13851e94e7a37
Alexander Smarus <bender@readdle.com>
2020-10-16 22:03:10 +0300

    [Windows] Remove obsolete TLS finalization
    Foundation uses Fls now. Fls provides automatic cleanup, and
    manual finalization on DLL_THREAD_DETACH is not needed.

--- CoreFoundation/Base.subproj/CFRuntime.c
+++ CoreFoundation/Base.subproj/CFRuntime.c
@@ -978,7 +978,6 @@ CF_PRIVATE void __CFTSDInitialize(void);
 #if TARGET_OS_WIN32
 // From CFPlatform.c
 CF_PRIVATE void __CFTSDWindowsCleanup(void);
-CF_PRIVATE void __CFFinalizeWindowsThreadData(void);
 #endif
 
 #if TARGET_OS_MAC
@@ -1352,8 +1351,6 @@ int DllMain( HINSTANCE hInstance, DWORD dwReason, LPVOID pReserved ) {
 	// do these last
 	if (cfBundle) CFRelease(cfBundle);
         __CFStringCleanup();
-    } else if (dwReason == DLL_THREAD_DETACH) {
-        __CFFinalizeWindowsThreadData();
     }
     return TRUE;
 }
