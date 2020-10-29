b878ee2c106d0883b2c1206aeffa61ca4a287c54
Karoy Lorentey <klorentey@apple.com>
2020-10-28 22:18:35 -0700

    Refresh file headers

--- Darwin/Foundation-swiftoverlay-Tests/TestDecimal.swift
+++ Darwin/Foundation-swiftoverlay-Tests/TestDecimal.swift
@@ -1,4 +1,8 @@
-// Copyright (c) 2014 - 2018 Apple Inc. and the Swift project authors
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2020 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
