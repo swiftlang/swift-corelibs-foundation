3aa51bda80d358d08b045337d66c4cfffb66d562
3405691582 <dsk@google.com>
2021-03-25 14:47:21 -0400

    Paper over some minor platform API differences.
    These may be better handled by some other mechanism, but for now, make
    these changes to get a clean compile:
    
    * getnameinfo on BSD takes a `size_t` for the host name,
      not a `socklen_t`.
    
    * PTHREAD_MUTEX_RECURSIVE is part of an enum on OpenBSD, so requires
      `.rawValue`.
    
    * Like on Android, SOCK_STREAM and SOCK_DGRAM are not enums.
      INADDR_LOOPBACK is a complex macro, so requires defining the flattened
      the value (this is of course brittle, but is the best option for now).

--- Sources/Foundation/Port.swift
+++ Sources/Foundation/Port.swift
@@ -83,19 +83,22 @@ public protocol PortDelegate: AnyObject {
     func handle(_ message: PortMessage)
 }
 
-#if canImport(Glibc) && !os(Android)
+#if canImport(Glibc) && !os(Android) && !os(OpenBSD)
 import Glibc
 fileprivate let SOCK_STREAM = Int32(Glibc.SOCK_STREAM.rawValue)
 fileprivate let SOCK_DGRAM  = Int32(Glibc.SOCK_DGRAM.rawValue)
 fileprivate let IPPROTO_TCP = Int32(Glibc.IPPROTO_TCP)
 #endif
 
-#if canImport(Glibc) && os(Android)
+#if canImport(Glibc) && os(Android) || os(OpenBSD)
 import Glibc
 fileprivate let SOCK_STREAM = Int32(Glibc.SOCK_STREAM)
 fileprivate let SOCK_DGRAM  = Int32(Glibc.SOCK_DGRAM)
 fileprivate let IPPROTO_TCP = Int32(Glibc.IPPROTO_TCP)
 fileprivate let INADDR_ANY: in_addr_t = 0
+#if os(OpenBSD)
+fileprivate let INADDR_LOOPBACK = 0x7f000001
+#endif
 #endif
 
 
--- Sources/Foundation/Thread.swift
+++ Sources/Foundation/Thread.swift
@@ -135,13 +135,13 @@ open class Thread : NSObject {
         var ti = end_at - start_at
         let end_ut = start_ut + ti
         while (0.0 < ti) {
-            var __ts__ = timespec(tv_sec: Int.max, tv_nsec: 0)
+            var __ts__ = timespec(tv_sec: time_t.max, tv_nsec: 0)
             if ti < Double(Int.max) {
                 var integ = 0.0
                 let frac: Double = withUnsafeMutablePointer(to: &integ) { integp in
                     return modf(ti, integp)
                 }
-                __ts__.tv_sec = Int(integ)
+                __ts__.tv_sec = time_t(integ)
                 __ts__.tv_nsec = Int(frac * 1000000000.0)
             }
             let _ = withUnsafePointer(to: &__ts__) { ts in
@@ -170,13 +170,13 @@ open class Thread : NSObject {
         let start_ut = CFGetSystemUptime()
         let end_ut = start_ut + ti
         while 0.0 < ti {
-            var __ts__ = timespec(tv_sec: Int.max, tv_nsec: 0)
+            var __ts__ = timespec(tv_sec: time_t.max, tv_nsec: 0)
             if ti < Double(Int.max) {
                 var integ = 0.0
                 let frac: Double = withUnsafeMutablePointer(to: &integ) { integp in
                     return modf(ti, integp)
                 }
-                __ts__.tv_sec = Int(integ)
+                __ts__.tv_sec = time_t(integ)
                 __ts__.tv_nsec = Int(frac * 1000000000.0)
             }
             let _ = withUnsafePointer(to: &__ts__) { ts in
