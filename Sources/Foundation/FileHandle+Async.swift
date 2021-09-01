2b572902042b1efd55df2ec3e4a2646ab6408f41
Aura Lily Vulcano <millenomi@apple.com>
2021-09-01 15:18:42 -0700

    Add missing availability.

--- Sources/Foundation/FileHandle+Async.swift
+++ Sources/Foundation/FileHandle+Async.swift
@@ -18,6 +18,7 @@ import Glibc
 import CRT
 #endif
 
+@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
 final actor IOActor {
     func read(from fd: Int32, into buffer: UnsafeMutableRawBufferPointer) async throws -> Int {
         while true {
