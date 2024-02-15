ad4867bb1814c41bbe5721d18abc51ae4519c9bf
Tony Parker <anthony.parker@apple.com>
2024-02-15 13:45:05 -0800

    Move Data support to NSData

--- Sources/Foundation/NSData.swift
+++ Sources/Foundation/NSData.swift
@@ -1205,3 +1205,68 @@ extension NSData {
         }
     }
 }
+
+// MARK: - Temporary URL support
+
+extension Data {
+    // Temporary until SwiftFoundation supports this
+    public init(contentsOf url: URL, options: ReadingOptions = []) throws {
+        self = try .init(contentsOf: url.path, options: options)
+    }
+
+    public func write(to url: URL, options: WritingOptions = []) throws {
+        try write(to: url.path, options: options)
+    }
+}
+
+// MARK: - Bridging
+
+extension Data {
+    @available(*, unavailable, renamed: "copyBytes(to:count:)")
+    public func getBytes<UnsafeMutablePointerVoid: _Pointer>(_ buffer: UnsafeMutablePointerVoid, length: Int) { }
+    
+    @available(*, unavailable, renamed: "copyBytes(to:from:)")
+    public func getBytes<UnsafeMutablePointerVoid: _Pointer>(_ buffer: UnsafeMutablePointerVoid, range: NSRange) { }
+}
+
+
+extension Data {
+    public init(referencing d: NSData) {
+        self = Data(d)
+    }
+}
+
+extension Data : _ObjectiveCBridgeable {
+    @_semantics("convertToObjectiveC")
+    public func _bridgeToObjectiveC() -> NSData {
+        return self.withUnsafeBytes {
+            NSData(bytes: $0.baseAddress, length: $0.count)
+        }
+    }
+    
+    public static func _forceBridgeFromObjectiveC(_ input: NSData, result: inout Data?) {
+        // We must copy the input because it might be mutable; just like storing a value type in ObjC
+        result = Data(input)
+    }
+    
+    public static func _conditionallyBridgeFromObjectiveC(_ input: NSData, result: inout Data?) -> Bool {
+        // We must copy the input because it might be mutable; just like storing a value type in ObjC
+        result = Data(input)
+        return true
+    }
+
+//    @_effects(readonly)
+    public static func _unconditionallyBridgeFromObjectiveC(_ source: NSData?) -> Data {
+        guard let src = source else { return Data() }
+        return Data(src)
+    }
+}
+
+extension NSData : _HasCustomAnyHashableRepresentation {
+    // Must be @nonobjc to avoid infinite recursion during bridging.
+    @nonobjc
+    public func _toCustomAnyHashable() -> AnyHashable? {
+        return AnyHashable(Data._unconditionallyBridgeFromObjectiveC(self))
+    }
+}
+
