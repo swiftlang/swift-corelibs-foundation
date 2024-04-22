4845b8bbf84b42e86ab31497d3a6ab13fae1bcf1
Olivier Martin <olivier@labapart.com>
2024-04-22 21:38:32 +0000

    CoreFoundation/URL.subproj: Added definitions for SSL key and certificate blob

--- CoreFoundation/URL.subproj/CFURLSessionInterface.c
+++ CoreFoundation/URL.subproj/CFURLSessionInterface.c
@@ -374,6 +374,9 @@ CFURLSessionOption const CFURLSessionOptionCOOKIE = { CURLOPT_COOKIE };
 CFURLSessionOption const CFURLSessionOptionHTTPHEADER = { CURLOPT_HTTPHEADER };
 CFURLSessionOption const CFURLSessionOptionHTTPPOST = { CURLOPT_HTTPPOST };
 CFURLSessionOption const CFURLSessionOptionSSLCERT = { CURLOPT_SSLCERT };
+#if !NS_CURL_MISSING_CURLINFO_SSLCERT_BLOB
+CFURLSessionOption const CFURLSessionOptionSSLCERT_BLOB = { CURLOPT_SSLCERT_BLOB };
+#endif
 CFURLSessionOption const CFURLSessionOptionKEYPASSWD = { CURLOPT_KEYPASSWD };
 CFURLSessionOption const CFURLSessionOptionCRLF = { CURLOPT_CRLF };
 CFURLSessionOption const CFURLSessionOptionQUOTE = { CURLOPT_QUOTE };
@@ -427,6 +430,9 @@ CFURLSessionOption const CFURLSessionOptionHTTP_VERSION = { CURLOPT_HTTP_VERSION
 CFURLSessionOption const CFURLSessionOptionFTP_USE_EPSV = { CURLOPT_FTP_USE_EPSV };
 CFURLSessionOption const CFURLSessionOptionSSLCERTTYPE = { CURLOPT_SSLCERTTYPE };
 CFURLSessionOption const CFURLSessionOptionSSLKEY = { CURLOPT_SSLKEY };
+#if !NS_CURL_MISSING_CURLINFO_SSLKEY_BLOB
+CFURLSessionOption const CFURLSessionOptionSSLKEY_BLOB = { CURLOPT_SSLKEY_BLOB };
+#endif
 CFURLSessionOption const CFURLSessionOptionSSLKEYTYPE = { CURLOPT_SSLKEYTYPE };
 CFURLSessionOption const CFURLSessionOptionSSLENGINE = { CURLOPT_SSLENGINE };
 CFURLSessionOption const CFURLSessionOptionSSLENGINE_DEFAULT = { CURLOPT_SSLENGINE_DEFAULT };
