b56e0679d12e0d6bf0e1504ec1c29d2bdc124899
Jonathan Flat <50605158+jrflat@users.noreply.github.com>
2024-06-06 08:24:51 -0700

    Add async URLSession methods (#4970)
    * Add `data(from:delegate:)` method.
    
    * Add async URLSession methods
    
    ---------
    
    Co-authored-by: ichiho <ichibon0@gmail.com>

--- Sources/FoundationNetworking/URLSession/TaskRegistry.swift
+++ Sources/FoundationNetworking/URLSession/TaskRegistry.swift
@@ -45,8 +45,12 @@ extension URLSession {
             case callDelegate
             /// Default action for all events, except for completion.
             case dataCompletionHandler(DataTaskCompletion)
+            /// Default action for all asynchronous events.
+            case dataCompletionHandlerWithTaskDelegate(DataTaskCompletion, URLSessionTaskDelegate?)
             /// Default action for all events, except for completion.
             case downloadCompletionHandler(DownloadTaskCompletion)
+            /// Default action for all asynchronous events.
+            case downloadCompletionHandlerWithTaskDelegate(DownloadTaskCompletion, URLSessionTaskDelegate?)
         }
         
         fileprivate var tasks: [Int: URLSessionTask] = [:]
