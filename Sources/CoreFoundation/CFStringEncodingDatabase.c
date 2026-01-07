12837421e7473cfcad061b79ef8f4d0d5f5c8636
AZero13 <gfunni234@gmail.com>
2026-01-07 13:57:42 -0500

    Simplify

--- Sources/CoreFoundation/CFStringEncodingDatabase.c
+++ Sources/CoreFoundation/CFStringEncodingDatabase.c
@@ -787,7 +787,8 @@ CF_PRIVATE CFStringEncoding __CFStringEncodingGetMostCompatibleMacScript(CFStrin
         case 0x0100: return kCFStringEncodingUnicode; break; // Unicode
 
         case 0x200: // ISO 8859
-            return ((((encoding & 0xFF) > 0) && ((encoding & 0xFF) <= (sizeof(__CFISO8859SimilarScriptList) / sizeof(*__CFISO8859SimilarScriptList)))) ? __CFISO8859SimilarScriptList[(encoding & 0xFF) - 1] : kCFStringEncodingInvalidId);
+            encoding &= 0xFF;
+            return (((encoding > 0) && (encoding <= (sizeof(__CFISO8859SimilarScriptList) / sizeof(*__CFISO8859SimilarScriptList)))) ? __CFISO8859SimilarScriptList[encoding - 1] : kCFStringEncodingInvalidId);
             break;
 
         default: {
