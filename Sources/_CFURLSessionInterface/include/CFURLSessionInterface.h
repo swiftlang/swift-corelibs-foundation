d94945992d676818e3ec87f4d7259eb017c97a56
Bassam (Sam) Khouri <bkhouri@users.noreply.github.com>
2026-01-15 08:33:32 -0500

    Revert "Serialize `curl_multi_cleanup` calls for OpenSSL <1.1.0 (#5321)"
    This reverts commit 95e1b22fd323778288b7d8beb4b2ff76416df54a.

--- Sources/_CFURLSessionInterface/include/CFURLSessionInterface.h
+++ Sources/_CFURLSessionInterface/include/CFURLSessionInterface.h
@@ -580,15 +580,6 @@ typedef struct CFURLSessionCurlVersion {
 } CFURLSessionCurlVersion;
 CF_EXPORT CFURLSessionCurlVersion CFURLSessionCurlVersionInfo(void);
 
-typedef struct CFURLSessionSSLVersion {
-    int major;
-    int minor;
-    int patch;
-    bool isOpenSSL;
-} CFURLSessionSSLVersion;
-
-CF_EXPORT CFURLSessionSSLVersion CFURLSessionSSLVersionInfo(void);
-
 
 CF_EXPORT int const CFURLSessionWriteFuncPause;
 CF_EXPORT int const CFURLSessionReadFuncPause;
